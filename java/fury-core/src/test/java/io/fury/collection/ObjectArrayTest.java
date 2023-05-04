a514446a9b273d48f5435b91bf0fbc98161cfde7
Shawn <shawn.ck.yang@gmail.com>
2023-05-04 18:01:33 +0800

    [Java] add auto-growing object array (#41)
    add auto-growing object array

---
+++ java/fury-core/src/test/java/io/fury/collection/ObjectArrayTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 The Fury authors
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fury.collection;
+
+import static org.testng.Assert.*;
+
+import java.util.stream.IntStream;
+import org.testng.annotations.Test;
+
+public class ObjectArrayTest {
+
+  @Test
+  public void testObjectArray() {
+    ObjectArray array = new ObjectArray(10);
+    for (int i = 0; i < 10; i++) {
+      array.add("abc");
+    }
+    assertEquals(array.objects, IntStream.range(0, 10).mapToObj(i -> "abc").toArray());
+    Object[] elementData = array.objects;
+    array.add(1);
+    assertNotSame(elementData, array.objects);
+  }
+}
