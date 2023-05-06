60d89fd1036c333c192b03992876400ca44fdcb0
Shawn <shawn.ck.yang@gmail.com>
2023-05-06 11:59:45 +0800

    [Java] add class info util (#83)
    add class info util

--- java/fury-core/src/main/java/io/fury/resolver/ClassInfo.java
+++ java/fury-core/src/main/java/io/fury/resolver/ClassInfo.java
@@ -18,4 +18,130 @@
 
 package io.fury.resolver;
 
-public class ClassInfo {}
+import com.google.common.base.Preconditions;
+import io.fury.Language;
+import io.fury.serializer.Serializer;
+import io.fury.util.Functions;
+import io.fury.util.ReflectionUtils;
+
+/**
+ * This class put together object type related information to reduce array/map loop up when
+ * serialization.
+ *
+ * @author chaokunyang
+ */
+public class ClassInfo {
+  final Class<?> cls;
+  final EnumStringBytes fullClassNameBytes;
+  final EnumStringBytes packageNameBytes;
+  final EnumStringBytes classNameBytes;
+  final boolean isDynamicGeneratedClass;
+  final EnumStringBytes typeTagBytes;
+  Serializer<?> serializer;
+  // use primitive to avoid boxing
+  short classId;
+
+  ClassInfo(
+      Class<?> cls,
+      EnumStringBytes fullClassNameBytes,
+      EnumStringBytes packageNameBytes,
+      EnumStringBytes classNameBytes,
+      boolean isDynamicGeneratedClass,
+      EnumStringBytes typeTagBytes,
+      Serializer<?> serializer,
+      short classId) {
+    this.cls = cls;
+    this.fullClassNameBytes = fullClassNameBytes;
+    this.packageNameBytes = packageNameBytes;
+    this.classNameBytes = classNameBytes;
+    this.isDynamicGeneratedClass = isDynamicGeneratedClass;
+    this.typeTagBytes = typeTagBytes;
+    this.serializer = serializer;
+    this.classId = classId;
+    if (cls != null && classId == ClassResolver.NO_CLASS_ID) {
+      Preconditions.checkArgument(classNameBytes != null);
+    }
+  }
+
+  ClassInfo(
+      ClassResolver classResolver,
+      Class<?> cls,
+      String tag,
+      Serializer<?> serializer,
+      short classId) {
+    this.cls = cls;
+    this.serializer = serializer;
+    EnumStringResolver enumStringResolver = classResolver.getEnumStringResolver();
+    if (cls != null && classResolver.getFury().getLanguage() != Language.JAVA) {
+      this.fullClassNameBytes = enumStringResolver.getOrCreateEnumStringBytes(cls.getName());
+    } else {
+      this.fullClassNameBytes = null;
+    }
+    if (cls != null && classId == ClassResolver.NO_CLASS_ID) {
+      String packageName = ReflectionUtils.getPackage(cls);
+      this.packageNameBytes = enumStringResolver.getOrCreateEnumStringBytes(packageName);
+      this.classNameBytes =
+          enumStringResolver.getOrCreateEnumStringBytes(
+              ReflectionUtils.getClassNameWithoutPackage(cls));
+    } else {
+      this.packageNameBytes = null;
+      this.classNameBytes = null;
+    }
+    if (tag != null) {
+      this.typeTagBytes = enumStringResolver.getOrCreateEnumStringBytes(tag);
+    } else {
+      this.typeTagBytes = null;
+    }
+    this.classId = classId;
+    if (cls != null) {
+      boolean isLambda = Functions.isLambda(cls);
+      boolean isProxy = ReflectionUtils.isJdkProxy(cls);
+      this.isDynamicGeneratedClass = isLambda || isProxy;
+      if (isLambda) {
+        this.classId = ClassResolver.LAMBDA_STUB_ID;
+      }
+      if (isProxy) {
+        this.classId = ClassResolver.JDK_PROXY_STUB_ID;
+      }
+    } else {
+      this.isDynamicGeneratedClass = false;
+    }
+  }
+
+  public Class<?> getCls() {
+    return cls;
+  }
+
+  public short getClassId() {
+    return classId;
+  }
+
+  public EnumStringBytes getPackageNameBytes() {
+    return packageNameBytes;
+  }
+
+  public EnumStringBytes getClassNameBytes() {
+    return classNameBytes;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> Serializer<T> getSerializer() {
+    return (Serializer<T>) serializer;
+  }
+
+  @Override
+  public String toString() {
+    return "ClassInfo{"
+        + "cls="
+        + cls
+        + ", fullClassNameBytes="
+        + fullClassNameBytes
+        + ", isDynamicGeneratedClass="
+        + isDynamicGeneratedClass
+        + ", serializer="
+        + serializer
+        + ", classId="
+        + classId
+        + '}';
+  }
+}
--- java/fury-core/src/main/java/io/fury/resolver/ClassResolver.java
+++ java/fury-core/src/main/java/io/fury/resolver/ClassResolver.java
@@ -18,14 +18,47 @@
 
 package io.fury.resolver;
 
+import io.fury.Fury;
 import io.fury.serializer.Serializer;
+import io.fury.util.LoggerFactory;
+import org.slf4j.Logger;
 
 @SuppressWarnings({"rawtypes", "unchecked", "UnstableApiUsage"})
 public class ClassResolver {
+  private static final Logger LOG = LoggerFactory.getLogger(ClassResolver.class);
+
+  public static final byte USE_CLASS_VALUE = 0;
+  public static final byte USE_STRING_ID = 1;
+  // preserve 0 as flag for class id not set in ClassInfo`
+  public static final short NO_CLASS_ID = (short) 0;
+  public static final short LAMBDA_STUB_ID = 1;
+  public static final short JDK_PROXY_STUB_ID = 2;
+  public static final short REPLACE_STUB_ID = 3;
 
   /** Get or create serializer for <code>cls</code>. */
   @SuppressWarnings("unchecked")
   public <T> Serializer<T> getSerializer(Class<T> cls) {
+    throw new UnsupportedOperationException();
+  }
+
+  public ClassInfo newClassInfo(Class<?> cls, Serializer<?> serializer, short classId) {
+    return new ClassInfo(this, cls, null, serializer, classId);
+  }
+
+  // Invoked by fury JIT.
+  public ClassInfo nilClassInfo() {
+    return new ClassInfo(this, null, null, null, NO_CLASS_ID);
+  }
+
+  public ClassInfoCache nilClassInfoCache() {
+    return new ClassInfoCache(nilClassInfo());
+  }
+
+  public EnumStringResolver getEnumStringResolver() {
+    throw new UnsupportedOperationException();
+  }
+
+  public Fury getFury() {
     throw new UnsupportedOperationException();
   }
 }
