7456bc66e42f614e3c839fa7c6c436c48dbd7b3a
chaokunyang <shawn.ck.yang@gmail.com>
2023-05-09 14:09:35 +0800

    implement Externalizable serialization

---
+++ java/fury-core/src/main/java/io/fury/serializer/ExternalizableSerializer.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 The Fury authors
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fury.serializer;
+
+import io.fury.Fury;
+import io.fury.io.FuryObjectInput;
+import io.fury.io.FuryObjectOutput;
+import io.fury.memory.MemoryBuffer;
+import io.fury.util.Platform;
+import io.fury.util.Utils;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Serializer for class implements {@link Externalizable}.
+ *
+ * @author chaokunyang
+ */
+public class ExternalizableSerializer<T extends Externalizable> extends Serializer<T> {
+  private Constructor<T> constructor;
+  private final FuryObjectInput objectInput;
+  private final FuryObjectOutput objectOutput;
+
+  public ExternalizableSerializer(Fury fury, Class<T> cls) {
+    super(fury, cls);
+    try {
+      constructor = cls.getConstructor();
+    } catch (NoSuchMethodException e) {
+      Utils.ignore(e);
+    }
+
+    objectInput = new FuryObjectInput(fury, null);
+    objectOutput = new FuryObjectOutput(fury, null);
+  }
+
+  @Override
+  public void write(MemoryBuffer buffer, T value) {
+    objectOutput.setBuffer(buffer);
+    try {
+      value.writeExternal(objectOutput);
+    } catch (IOException e) {
+      Platform.throwException(e);
+    }
+  }
+
+  @Override
+  public void crossLanguageWrite(MemoryBuffer buffer, T value) {
+    throw new UnsupportedOperationException("Externalizable can only be used in java");
+  }
+
+  @Override
+  public T read(MemoryBuffer buffer) {
+    T t;
+    if (constructor != null) {
+      try {
+        t = constructor.newInstance();
+      } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      t = Platform.newInstance(type);
+    }
+    objectInput.setBuffer(buffer);
+    try {
+      t.readExternal(objectInput);
+    } catch (IOException | ClassNotFoundException e) {
+      Platform.throwException(e);
+    }
+    return t;
+  }
+}
