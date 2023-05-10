a9f751aa9377623700eba4100ff2f17bba3c36e6
chaokunyang <shawn.ck.yang@gmail.com>
2023-05-10 20:34:15 +0800

    fix format

--- java/fury-core/src/main/java/io/fury/serializer/UnmodifiableSerializers.java
+++ java/fury-core/src/main/java/io/fury/serializer/UnmodifiableSerializers.java
@@ -29,6 +29,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.fury.serializer;
 
 import com.google.common.base.Preconditions;
