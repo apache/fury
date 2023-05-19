9a28e30c9ad75bef413777e148ff22b8693ed757
chaokunyang <shawn.ck.yang@gmail.com>
2023-05-19 11:12:29 +0800

    add todo serializers

---
+++ java/fury-core/src/main/java/io/fury/serializer/GuavaSerializers.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 The Fury authors
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fury.serializer;
+
+public class GuavaSerializers {
+
+  // TODO guava serializers
+  // guava/ArrayListMultimapSerializer - serializer for guava-libraries' ArrayListMultimap
+  // guava/ArrayTableSerializer - serializer for guava-libraries' ArrayTable
+  // guava/HashBasedTableSerializer - serializer for guava-libraries' HashBasedTable
+  // guava/HashMultimapSerializer -- serializer for guava-libraries' HashMultimap
+  // guava/ImmutableListSerializer - serializer for guava-libraries' ImmutableList
+  // guava/ImmutableSetSerializer - serializer for guava-libraries' ImmutableSet
+  // guava/ImmutableMapSerializer - serializer for guava-libraries' ImmutableMap
+  // guava/ImmutableMultimapSerializer - serializer for guava-libraries' ImmutableMultimap
+  // guava/ImmutableSortedSetSerializer - serializer for guava-libraries' ImmutableSortedSet
+  // guava/ImmutableTableSerializer - serializer for guava-libraries' ImmutableTable
+  // guava/LinkedHashMultimapSerializer - serializer for guava-libraries' LinkedHashMultimap
+  // guava/LinkedListMultimapSerializer - serializer for guava-libraries' LinkedListMultimap
+  // guava/ReverseListSerializer - serializer for guava-libraries' Lists.ReverseList / Lists.reverse
+  // guava/TreeBasedTableSerializer - serializer for guava-libraries' TreeBasedTable
+  // guava/TreeMultimapSerializer - serializer for guava-libraries' TreeMultimap
+  // guava/UnmodifiableNavigableSetSerializer - serializer for guava-libraries'
+  // UnmodifiableNavigableSet
+
+}
