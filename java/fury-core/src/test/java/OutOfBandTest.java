3f4dba51b6f1c31e3b315c3e0f149d4e5dfcda2a
chaokunyang <shawn.ck.yang@gmail.com>
2023-05-19 14:32:40 +0800

    add zero-copy tests

---
+++ java/fury-core/src/test/java/OutOfBandTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 The Fury authors
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.fury.Fury;
+import io.fury.FuryTestBase;
+import io.fury.Language;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.testng.Assert.assertEquals;
+
+public class OutOfBandTest extends FuryTestBase {
+
+  @DataProvider(name = "referenceTrackingConfig")
+  public static Object[][] referenceTrackingConfig() {
+    return new Object[][] {{false}, {true}};
+  }
+
+  @Test(dataProvider = "referenceTrackingConfig")
+  public void testByteBuffer(boolean referenceTracking) {
+    Fury.FuryBuilder builder =
+        Fury.builder()
+            .withLanguage(Language.JAVA)
+            .withReferenceTracking(referenceTracking)
+            .disableSecureMode();
+    Fury fury1 = builder.build();
+    Fury fury2 = builder.build();
+    ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+    byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    byteBuffer.putInt(100);
+    byteBuffer.rewind();
+    AtomicInteger counter = new AtomicInteger(0);
+    assertEquals(serDeOutOfBand(counter, fury1, fury1, byteBuffer), byteBuffer);
+    assertEquals(serDeOutOfBand(counter, fury1, fury2, byteBuffer), byteBuffer);
+    byteBuffer.order(ByteOrder.BIG_ENDIAN);
+    assertEquals(serDeOutOfBand(counter, fury1, fury1, byteBuffer), byteBuffer);
+    assertEquals(serDeOutOfBand(counter, fury1, fury2, byteBuffer), byteBuffer);
+  }
+}
