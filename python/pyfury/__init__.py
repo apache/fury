6b9aa0dfec187a632abaf36e74bd7a9efb1f3533
Shawn <shawn.ck.yang@gmail.com>
2023-07-01 23:18:16 +0800

    [Python] Unify cython python serializer (#507)
    * cython build
    
    * unify cython/python serializers
    
    * clean code

--- python/pyfury/__init__.py
+++ python/pyfury/__init__.py
@@ -1,3 +1,34 @@
+from pyfury import lib  # noqa: F401 # pylint: disable=unused-import
+from pyfury._fury import (  # noqa: F401 # pylint: disable=unused-import
+    Fury,
+    Language,
+    OpaqueObject,
+)
+
+try:
+    from pyfury._serialization import ENABLE_FURY_CYTHON_SERIALIZATION
+except ImportError:
+    ENABLE_FURY_CYTHON_SERIALIZATION = False
+
+if ENABLE_FURY_CYTHON_SERIALIZATION:
+    from pyfury._serialization import (  # noqa: F401,F811
+        Fury,
+        Language,
+        ClassInfo,
+        OpaqueObject,
+        ComplexObjectSerializer,
+    )
+else:
+    from pyfury._fury import (  # noqa: F401,F403,F811 # pylint: disable=unused-import
+        Fury,
+        Language,
+        ClassInfo,
+        OpaqueObject,
+    )
+    from pyfury._struct import (  # noqa: F401,F403,F811 # pylint: disable=unused-import
+        ComplexObjectSerializer,
+    )
+from pyfury.serializer import *  # noqa: F401,F403 # pylint: disable=unused-import
 from pyfury.type import (  # noqa: F401 # pylint: disable=unused-import
     record_class_factory,
     get_qualified_classname,
@@ -20,8 +51,7 @@ from pyfury._util import Buffer  # noqa: F401 # pylint: disable=unused-import
 
 try:
     from pyfury.format import *  # noqa: F401,F403 # pylint: disable=unused-import
-except (AttributeError, ImportError) as e:
-    print(e)
-    raise
+except (AttributeError, ImportError):
+    pass
 
 __version__ = "0.1.0.dev"
