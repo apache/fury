7475d773fe88c8d0784266d93ec8181b02df68ab
chaokunyang <shawn.ck.yang@gmail.com>
2023-07-13 00:11:13 +0800

    remove requireClassRegistration from Fury

--- java/fury-core/src/main/java/io/fury/Fury.java
+++ java/fury-core/src/main/java/io/fury/Fury.java
@@ -1244,10 +1244,6 @@ public final class Fury {
     return compressNumber;
   }
 
-  public boolean requireClassRegistration() {
-    return config.requireClassRegistration();
-  }
-
   public static FuryBuilder builder() {
     return new FuryBuilder();
   }
