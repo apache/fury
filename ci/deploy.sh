6d891e1c8da174e7431c85984a4df78a075c1c86
chaokunyang <shawn.ck.yang@gmail.com>
2023-07-18 15:15:30 +0800

    add deploy_jars function

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -76,7 +76,17 @@ bump_javascript_version() {
   version=$1
   cd "$ROOT/javascript"
   echo "Set fury javascript version to $version"
+  pushd packages/fury
   sed -i '' -E "s/\"version\": .*,/\"version\": \"$version\",/" package.json
+  popd
+  pushd packages/hps
+  sed -i '' -E "s/\"version\": .*,/\"version\": \"$version\",/" package.json
+  popd
+}
+
+deploy_jars() {
+  cd "$ROOT/java"
+  mvn -T10 clean deploy -DskipTests -Prelease
 }
 
 deploy_python() {
