4aa18fa20c6839a49efbd3cb021bcf6434500234
chaokunyang <shawn.ck.yang@gmail.com>
2023-07-25 01:03:04 +0800

    remove guava dispatch

--- java/fury-core/src/main/java/io/fury/resolver/ClassResolver.java
+++ java/fury-core/src/main/java/io/fury/resolver/ClassResolver.java
@@ -28,9 +28,7 @@ import static io.fury.type.TypeUtils.PRIMITIVE_SHORT_TYPE;
 import static io.fury.type.TypeUtils.getRawType;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.primitives.Primitives;
 import com.google.common.reflect.TypeToken;
 import io.fury.Fury;
@@ -721,12 +719,6 @@ public class ClassResolver {
         return TimeSerializers.TimeZoneSerializer.class;
       } else if (Externalizable.class.isAssignableFrom(cls)) {
         return ExternalizableSerializer.class;
-      } else if (ImmutableList.class.isAssignableFrom(cls)) {
-        return GuavaSerializers.ImmutableListSerializer.class;
-      } else if (ImmutableSet.class.isAssignableFrom(cls)) {
-        return GuavaSerializers.ImmutableSetSerializer.class;
-      } else if (ImmutableMap.class.isAssignableFrom(cls)) {
-        return GuavaSerializers.ImmutableMapSerializer.class;
       } else if (ByteBuffer.class.isAssignableFrom(cls)) {
         return BufferSerializers.ByteBufferSerializer.class;
       }
