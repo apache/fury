6f286b2e6f4d32e2ea140f4e65b9924189e8f5e2
chaokunyang <shawn.ck.yang@gmail.com>
2023-08-06 18:48:01 +0800

    test record generics

--- integration_tests/latest_jdk_tests/src/test/java/io/fury/integration_tests/RecordSerializersTest.java
+++ integration_tests/latest_jdk_tests/src/test/java/io/fury/integration_tests/RecordSerializersTest.java
@@ -5,9 +5,14 @@ import io.fury.util.RecordUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RecordSerializerTest {
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
 
-  public record Foo(int f1, String f2) {}
+public class RecordSerializersTest {
+
+  public record Foo(int f1, String f2, List<String> f3) {
+  }
 
   @Test
   public void testIsRecord() {
@@ -23,5 +28,14 @@ public class RecordSerializerTest {
     Assert.assertEquals(recordComponents[0].getDeclaringRecord(), expectComponents[0].getDeclaringRecord());
     Assert.assertEquals(recordComponents[0].getType(), expectComponents[0].getType());
     Assert.assertEquals(recordComponents[0].getName(), expectComponents[0].getName());
+  }
+
+  @Test
+  public void testGetRecordGenerics() {
+    RecordComponent[] recordComponents = RecordUtils.getRecordComponents(Foo.class);
+    Assert.assertNotNull(recordComponents);
+    Type genericType = recordComponents[2].getGenericType();
+    ParameterizedType parameterizedType = (ParameterizedType) genericType;
+    Assert.assertEquals(parameterizedType.getActualTypeArguments()[0], String.class);
   }
 }
