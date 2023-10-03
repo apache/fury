0f3fdab11ae9be45d63136f4deca0ff1b92e1a5f
chaokunyang <shawn.ck.yang@gmail.com>
2023-10-03 22:33:24 +0800

    add CollectionSuite header

--- java/fury-benchmark/src/main/java/io/fury/benchmark/CollectionSuite.java
+++ java/fury-benchmark/src/main/java/io/fury/benchmark/CollectionSuite.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 The Fury Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fury.benchmark;
 
 import io.fury.Fury;
@@ -9,6 +25,11 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Test suite for collection.
+ *
+ * @author chaokunyang
+ */
 public class CollectionSuite {
   private static final Logger LOG = LoggerFactory.getLogger(CollectionSuite.class);
 
