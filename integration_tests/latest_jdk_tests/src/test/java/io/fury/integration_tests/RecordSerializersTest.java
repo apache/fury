b9e1b1912736036cd9f702f98706f84d8f22b69f
Shawn <shawn.ck.yang@gmail.com>
2023-10-12 01:57:37 +0800

    [Java] Fix private record JIT (#1004)
    * fix private record jit
    
    * add codegen param
    
    * lint code

--- integration_tests/latest_jdk_tests/src/test/java/io/fury/integration_tests/RecordSerializersTest.java
+++ integration_tests/latest_jdk_tests/src/test/java/io/fury/integration_tests/RecordSerializersTest.java
@@ -219,4 +219,16 @@ public class RecordSerializersTest {
       Assert.assertEquals(fury.deserialize(bytes), o2);
     }
   }
+
+  @Test(dataProvider = "codegen")
+  public void testPrivateRecord(boolean codegen) {
+    Fury fury = Fury.builder().withCodegen(codegen).build();
+    fury.register(PrivateRecord.class);
+    byte[] serialized = fury.serialize(new PrivateRecord("foo")); // fails
+    Object deserialized = fury.deserialize(serialized);
+    System.out.println(deserialized);
+  }
+
+  private record PrivateRecord(String foo) {
+  }
 }
