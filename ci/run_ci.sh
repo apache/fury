b1f85f6dabb72bcfdd18c961148274307a496bd4
chaokunyang <shawn.ck.yang@gmail.com>
2023-11-21 20:03:25 +0800

    fix path

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -25,11 +25,11 @@ install_python() {
   wget -q https://repo.anaconda.com/miniconda/Miniconda3-py38_23.5.2-0-Linux-x86_64.sh -O Miniconda3.sh
   bash Miniconda3.sh -b -p $HOME/miniconda && rm -f miniconda.*
   which python
-  echo "Python version $(python -V)"
+  echo "Python version $(python -V), path $(which python)"
 }
 
 install_pyfury() {
-  echo "Python version $(python -V)"
+  echo "Python version $(python -V), path $(which python)"
   "$ROOT"/ci/deploy.sh install_pyarrow
   pip install Cython wheel numpy pytest
   pushd "$ROOT/python"
