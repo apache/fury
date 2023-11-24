91954246bc9126b28e3c821e1e5e2f385994f048
chaokunyang <shawn.ck.yang@gmail.com>
2023-11-24 21:27:24 +0800

    fix scala collection serialization nested in pojo

--- scala/src/test/scala/io/fury/serializer/CollectionSerializerTest.scala
+++ scala/src/test/scala/io/fury/serializer/CollectionSerializerTest.scala
@@ -23,67 +23,45 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class CollectionSerializerTest extends AnyWordSpec with Matchers {
-  val fury1: Fury = Fury.builder()
-    .withLanguage(Language.JAVA)
-    .withRefTracking(true)
-    .withScalaOptimizationEnabled(false)
-    .requireClassRegistration(false).build()
-  fury1.getClassResolver.setSerializerFactory(null)
-  val fury2: Fury = Fury.builder()
-    .withLanguage(Language.JAVA)
-    .withRefTracking(true)
-    .withScalaOptimizationEnabled(true)
-    .requireClassRegistration(false).build()
-  fury2.getClassResolver.setSerializerFactory(new ScalaDispatcher())
-
-  "fury scala collection support noopt" should {
-    "serialize/deserialize Seq" in {
-      val seq = Seq(100, 10000L)
-      fury1.deserialize(fury1.serialize(seq)) shouldEqual seq
+  val params: Seq[(Boolean, Boolean)] = List((false, false), (false, true), (true, false), (true, true))
+  params.foreach{case (setOpt, setFactory) => {
+    val fury1: Fury = Fury.builder()
+      .withLanguage(Language.JAVA)
+      .withRefTracking(true)
+      .withScalaOptimizationEnabled(setOpt)
+      .requireClassRegistration(false).build()
+    if (setFactory) {
+      fury1.getClassResolver.setSerializerFactory(new ScalaDispatcher())
     }
-    "serialize/deserialize List" in {
-      val list = List(100, 10000L)
-      fury1.deserialize(fury1.serialize(list)) shouldEqual list
+    s"fury scala collection support: setOpt $setOpt, setFactory $setFactory" should {
+      "serialize/deserialize Seq" in {
+        val seq = Seq(100, 10000L)
+        fury1.deserialize(fury1.serialize(seq)) shouldEqual seq
+      }
+      "serialize/deserialize List" in {
+        val list = List(100, 10000L)
+        fury1.deserialize(fury1.serialize(list)) shouldEqual list
+      }
+      "serialize/deserialize Set" in {
+        val set = Set(100, 10000L)
+        fury1.deserialize(fury1.serialize(set)) shouldEqual set
+      }
+      "serialize/deserialize CollectionStruct1" in {
+        val struct = CollectionStruct1(List("a", "b"))
+        fury1.deserialize(fury1.serialize(struct)) shouldEqual struct
+      }
     }
-    "serialize/deserialize Set" in {
-      val set = Set(100, 10000L)
-      fury1.deserialize(fury1.serialize(set)) shouldEqual set
+    s"fury scala map support: setOpt $setOpt, setFactory $setFactory" should {
+      "serialize/deserialize Map" in {
+        val map = Map("a" -> 100, "b" -> 10000L)
+        fury1.deserialize(fury1.serialize(map)) shouldEqual map
+      }
+      "serialize/deserialize MapStruct1" in {
+        val struct = MapStruct1(Map("k1" -> "v1", "k2" -> "v2"))
+        fury1.deserialize(fury1.serialize(struct)) shouldEqual struct
+      }
     }
-  }
-  "fury scala collection support opt" should {
-    "serialize/deserialize Seq" in {
-      val seq = Seq(100, 10000L)
-      fury2.deserialize(fury2.serialize(seq)) shouldEqual seq
-    }
-    "serialize/deserialize List" in {
-      val list = List(100, 10000L)
-      fury2.deserialize(fury2.serialize(list)) shouldEqual list
-    }
-    "serialize/deserialize Set" in {
-      val set = Set(100, 10000L)
-      fury2.deserialize(fury2.serialize(set)) shouldEqual set
-    }
-    "serialize/deserialize CollectionStruct1" in {
-      val struct = CollectionStruct1(List("a", "b"))
-      fury2.deserialize(fury2.serialize(struct)) shouldEqual struct
-    }
-  }
-  "fury scala map support noopt" should {
-    "serialize/deserialize Map" in {
-      val map = Map("a" -> 100, "b" -> 10000L)
-      fury1.deserialize(fury1.serialize(map)) shouldEqual map
-    }
-  }
-  "fury scala map support opt" should {
-    "serialize/deserialize Map" in {
-      val map = Map("a" -> 100, "b" -> 10000L)
-      fury2.deserialize(fury2.serialize(map)) shouldEqual map
-    }
-    "serialize/deserialize MapStruct1" in {
-      val struct = MapStruct1(Map("k1" -> "v1", "k2" -> "v2"))
-      fury2.deserialize(fury2.serialize(struct)) shouldEqual struct
-    }
-  }
+  }}
 }
 
 case class CollectionStruct1(list: List[String])
