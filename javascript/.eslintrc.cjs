32ae33f72e84edfae8e5c75148653aa88d618ea6
artin <artin@cat.ms>
2023-12-15 16:24:04 +0800

    [JavaScript] Add lint tools

--- javascript/.eslintrc.cjs
+++ javascript/.eslintrc.cjs
@@ -1,12 +1,22 @@
+const stylistic = require('@stylistic/eslint-plugin')
+
+const customized = stylistic.configs.customize({
+  indent: 2,
+  quotes: 'double',
+  semi: true,
+  jsx: true,
+});
+
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', '@stylistic'],
   root: true,
   rules: {
+    ...customized.rules,
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
   },
-  ignorePatterns: ["test/*", "dist/*", "*.js", "murmurHash3.ts"]
+  ignorePatterns: ["test/*", "dist/*", "*.js", "murmurHash3.ts", "packages/**/dist/*", "packages/**/build/*"]
 };
