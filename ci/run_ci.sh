0301dd86e900601794d8012325b1453a1627a078
chaokunyang <shawn.ck.yang@gmail.com>
2024-01-07 22:13:44 +0800

    rename fury-benchmark to benchmark

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -110,7 +110,7 @@ integration_tests() {
   cd "$ROOT"/java
   mvn -T10 -B --no-transfer-progress clean install -DskipTests
   echo "benchmark tests"
-  cd "$ROOT"/java/fury-benchmark
+  cd "$ROOT"/java/benchmark
   mvn -T10 -B --no-transfer-progress clean test -Pjmh
   echo "Start latest jdk tests"
   cd "$ROOT"/integration_tests/latest_jdk_tests
