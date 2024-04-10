bac9e27f6d8e85216430776a6e1fbf3dacab9558
chaokunyang <shawn.ck.yang@gmail.com>
2024-04-10 13:58:56 +0800

    lint code

--- integration_tests/jdk_compatibility_tests/src/test/java/org/apache/fury/integration_tests/JDKCompatibilityTest.java
+++ integration_tests/jdk_compatibility_tests/src/test/java/org/apache/fury/integration_tests/JDKCompatibilityTest.java
@@ -29,7 +29,6 @@ import org.apache.fury.Fury;
 import org.apache.fury.config.CompatibleMode;
 import org.apache.fury.config.FuryBuilder;
 import org.apache.fury.config.Language;
-import org.apache.fury.test.bean.BeanA;
 import org.apache.fury.util.Platform;
 import org.testng.Assert;
 import org.testng.annotations.Test;
