187005b082f790d7c5f26ac8428c87917522f423
chaokunyang <shawn.ck.yang@gmail.com>
2024-04-13 15:01:01 +0800

    use readVarUint32Small7 for most small size

--- java/fury-core/src/main/java/org/apache/fury/type/ClassDef.java
+++ java/fury-core/src/main/java/org/apache/fury/type/ClassDef.java
@@ -234,7 +234,7 @@ public class ClassDef implements Serializable {
   private static String readSharedString(MemoryBuffer buffer, List<String> strings) {
     String str;
     if (buffer.readBoolean()) {
-      return strings.get(buffer.readVarUint32Small14());
+      return strings.get(buffer.readVarUint32Small7());
     } else {
       str = new String(buffer.readBytesAndSize(), StandardCharsets.UTF_8);
       strings.add(str);
