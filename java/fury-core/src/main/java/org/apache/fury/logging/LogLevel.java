ba15fcb0a165b37085ae285a6f62fa0e33e84d86
LiangliangSui <coolsui.coding@gmail.com>
2024-04-14 11:31:38 +0800

    lint code.
    Signed-off-by: LiangliangSui <coolsui.coding@gmail.com>

--- java/fury-core/src/main/java/org/apache/fury/logging/LogLevel.java
+++ java/fury-core/src/main/java/org/apache/fury/logging/LogLevel.java
@@ -19,7 +19,7 @@
 
 package org.apache.fury.logging;
 
-/** Defines a series of log levels */
+/** Defines a series of log levels. */
 public class LogLevel {
   public static final int ERROR_LEVEL = 0;
 
