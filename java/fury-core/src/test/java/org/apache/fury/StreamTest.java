apache/fury

commit fd424b85ad33db609f40c9705dbcb52be0ccdcae

feat(java): native streaming mode deserialization (#1451)
## What this PR do?
This PR implements streaming mode deserialization, which make the
protocol between `Fury#serialize(buffer, o)` and `Fury#serialize(stream,
o)` consistent, and make the deserialization support real streaming mode
to reduce latency, and facilitate the pipeline between deserialization
and transfer.

The changes inlucde:
- Introduce a `FuryStreamReader` to allow buffered streaming reading.
- Make `MemoryBuffer` support streaming reading without introduce cost
on critical path.
- Implements `InputStream` based `FuryStreamReader` for Fury.

`java.nio.Channel` based zero-copy is not implemented in this PR.

## Related issues
Closes #1435

## Media Benchmark
with this PR:
```
Benchmark                                  (bufferType)   (objectType)  (references)   Mode  Cnt        Score       Error  Units
UserTypeDeserializeSuite.fury_deserialize         array  MEDIA_CONTENT         false  thrpt   50  2867721.514 ± 55102.830  ops/s
UserTypeDeserializeSuite.fury_deserialize         array  MEDIA_CONTENT          true  thrpt   50  1841586.657 ± 46192.190  ops/s
UserTypeDeserializeSuite.fury_deserialize  directBuffer  MEDIA_CONTENT         false  thrpt   50  2542987.512 ± 49835.129  ops/s
UserTypeDeserializeSuite.fury_deserialize  directBuffer  MEDIA_CONTENT          true  thrpt   50  1712919.362 ± 26500.068  ops/s
```
Before this PR: 4c60eb9780ec51ffe1aac3ee2da594c0762799e0
```
Benchmark                                  (bufferType)   (objectType)  (references)   Mode  Cnt        Score       Error  Units
UserTypeDeserializeSuite.fury_deserialize         array  MEDIA_CONTENT         false  thrpt   50  2971081.559 ± 52348.975  ops/s
UserTypeDeserializeSuite.fury_deserialize         array  MEDIA_CONTENT          true  thrpt   50  1938252.031 ± 25077.333  ops/s

java/fury-core/src/main/java/org/apache/fury/resolver/ClassResolver.java
@@ -1292,7 +1292,7 @@ public class ClassResolver {
         metaContext,
         "Meta context must be set before serialization,"
             + " please set meta context by SerializationContext.setMetaContext");
-    int id = buffer.readPositiveVarInt();
+    int id = buffer.readVarUintSmall();
     List<ClassInfo> readClassInfos = metaContext.readClassInfos;
     ClassInfo classInfo = readClassInfos.get(id);
     if (classInfo == null) {
@@ -1315,7 +1315,7 @@ public class ClassResolver {
         metaContext,
         "Meta context must be set before serialization,"
             + " please set meta context by SerializationContext.setMetaContext");
-    int id = buffer.readPositiveVarInt();
+    int id = buffer.readVarUintSmall();
     List<ClassInfo> readClassInfos = metaContext.readClassInfos;
     ClassInfo classInfo = readClassInfos.get(id);
     if (classInfo == null) {
@@ -1399,7 +1399,7 @@ public class ClassResolver {
     int classDefOffset = buffer.readInt();
     int readerIndex = buffer.readerIndex();
     buffer.readerIndex(classDefOffset);
-    int numClassDefs = buffer.readPositiveVarInt();
+    int numClassDefs = buffer.readVarUintSmall();
     for (int i = 0; i < numClassDefs; i++) {
       ClassDef readClassDef = ClassDef.readClassDef(buffer);
       // Share same class def to reduce memory footprint, since there may be many meta context.
@@ -1468,7 +1468,7 @@ public class ClassResolver {
 
   // Note: Thread safe fot jit thread to call.
   public Expression skipRegisteredClassExpr(Expression buffer) {
-    return new Invoke(buffer, "readPositiveVarInt");
+    return new Invoke(buffer, "readVarUintSmall");
   }
 
   /**
@@ -1538,8 +1538,7 @@ public class ClassResolver {
       currentReadClass = classInfo.cls;
       return classInfo;
     } else {
-      short classId = readClassId(buffer, flag);
-      ClassInfo classInfo = getOrUpdateClassInfo(classId);
+      ClassInfo classInfo = getOrUpdateClassInfo(readClassId(buffer, flag));
       currentReadClass = classInfo.cls;
       return classInfo;
     }
@@ -1582,8 +1581,8 @@ public class ClassResolver {
     short classId;
     // use classId
     if ((flag & 0x80) != 0) { // class id is written using multiple bytes.
-      buffer.increaseReaderIndexUnsafe(-1);
-      classId = (short) buffer.readPositiveVarInt();
+      buffer.increaseReaderIndex(-1);
+      classId = (short) buffer.readVarUintSmall();
     } else {
       classId = (short) (flag & 0x7F);
     }

commit 5d777be7a675edaf01c19e2c7f88ac74ff4fdec6

feat(java): support meta string encoding for classname and package name (#1527)
## What does this PR do?
This PR supports meta string encoding for classname and package name in
fury java serialization.

Before this PR a unregistered class serialization:
```
Fury | MEDIA_CONTENT | false | array | 405 |
```

With this PR:
```
Fury | MEDIA_CONTENT | false | array | 390 |
```

This PR also fix buffer streaming readSliInt64 bugs introduced in #1451 

## Related issues

<!--
Is there any related issue? Please attach here.

- #xxxx0
- #xxxx1
- #xxxx2
-->


## Does this PR introduce any user-facing change?

<!--
If any user-facing interface changes, please [open an
issue](https://github.com/apache/incubator-fury/issues/new/choose)
describing the need to do so and update the document if necessary.
-->

- [ ] Does this PR introduce any public API change?
- [ ] Does this PR introduce any binary protocol compatibility change?


## Benchmark

<!--
When the PR has an impact on performance (if you don't know whether the
PR will have an impact on performance, you can submit the PR first, and
if it will have impact on performance, the code reviewer will explain
it), be sure to attach a benchmark data here.
-->

go/fury/type.go
@@ -557,7 +557,7 @@ func (r *typeResolver) writeMetaString(buffer *ByteBuffer, str string) error {
 		if _, err := h.Write([]byte(str)); err != nil {
 			return err
 		}
-		hash := int64(h.Sum64())
+		hash := int64(h.Sum64() & 0xffffffffffffff00)
 		buffer.WriteInt64(hash)
 		if len(str) > MaxInt16 {
 			return fmt.Errorf("too long string: %s", str)

java/fury-core/src/main/java/org/apache/fury/memory/MemoryBuffer.java
@@ -1471,6 +1471,7 @@ public final class MemoryBuffer {
       readerIndex = readIdx + 4;
       return i >> 1;
     }
+    diff = size - readIdx;
     if (diff < 9) {
       streamReader.fillBuffer(9 - diff);
     }
@@ -1493,6 +1494,7 @@ public final class MemoryBuffer {
       readerIndex = readIdx + 4;
       return i >> 1;
     }
+    diff = size - readIdx;
     if (diff < 9) {
       streamReader.fillBuffer(9 - diff);
     }
@@ -2511,8 +2513,6 @@ public final class MemoryBuffer {
         + writerIndex
         + ", heapMemory="
         + (heapMemory == null ? null : "len(" + heapMemory.length + ")")
-        + ", heapData="
-        + Arrays.toString(heapMemory)
         + ", heapOffset="
         + heapOffset
         + ", offHeapBuffer="

java/fury-core/src/main/java/org/apache/fury/meta/MetaString.java
@@ -30,11 +30,11 @@ import java.util.Objects;
 public class MetaString {
   /** Defines the types of supported encodings for MetaStrings. */
   public enum Encoding {
-    LOWER_SPECIAL(0x00),
-    LOWER_UPPER_DIGIT_SPECIAL(0x01),
-    FIRST_TO_LOWER_SPECIAL(0x02),
-    ALL_TO_LOWER_SPECIAL(0x03),
-    UTF_8(0x04); // Using UTF-8 as the fallback
+    UTF_8(0x00), // Using UTF-8 as the fallback
+    LOWER_SPECIAL(0x01),
+    LOWER_UPPER_DIGIT_SPECIAL(0x02),
+    FIRST_TO_LOWER_SPECIAL(0x03),
+    ALL_TO_LOWER_SPECIAL(0x04);
 
     private final int value;
 
@@ -61,6 +61,7 @@ public class MetaString {
   private final char specialChar1;
   private final char specialChar2;
   private final byte[] bytes;
+  private final int numChars;
   private final int numBits;
 
   /**
@@ -76,12 +77,14 @@ public class MetaString {
       char specialChar1,
       char specialChar2,
       byte[] bytes,
+      int numChars,
       int numBits) {
     this.string = string;
     this.encoding = encoding;
     this.specialChar1 = specialChar1;
     this.specialChar2 = specialChar2;
     this.bytes = bytes;
+    this.numChars = numChars;
     this.numBits = numBits;
   }
 
@@ -105,6 +108,10 @@ public class MetaString {
     return bytes;
   }
 
+  public int getNumChars() {
+    return numChars;
+  }
+
   public int getNumBits() {
     return numBits;
   }
@@ -120,6 +127,7 @@ public class MetaString {
     MetaString that = (MetaString) o;
     return specialChar1 == that.specialChar1
         && specialChar2 == that.specialChar2
+        && numChars == that.numChars
         && numBits == that.numBits
         && encoding == that.encoding
         && Arrays.equals(bytes, that.bytes);
@@ -127,7 +135,7 @@ public class MetaString {
 
   @Override
   public int hashCode() {
-    int result = Objects.hash(encoding, specialChar1, specialChar2, numBits);
+    int result = Objects.hash(encoding, specialChar1, specialChar2, numChars, numBits);
     result = 31 * result + Arrays.hashCode(bytes);
     return result;
   }
@@ -145,6 +153,8 @@ public class MetaString {
         + specialChar2
         + ", bytes="
         + Arrays.toString(bytes)
+        + ", numChars="
+        + numChars
         + ", numBits="
         + numBits
         + '}';

java/fury-core/src/main/java/org/apache/fury/meta/MetaStringDecoder.java
@@ -20,7 +20,6 @@
 package org.apache.fury.meta;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import org.apache.fury.meta.MetaString.Encoding;
 import org.apache.fury.util.StringUtils;
 
@@ -38,25 +37,6 @@ public class MetaStringDecoder {
   public MetaStringDecoder(char specialChar1, char specialChar2) {
     this.specialChar1 = specialChar1;
     this.specialChar2 = specialChar2;
-  }
-
-  /**
-   * Decodes the encoded data back into a string based on the provided number of bits and encoding
-   * type.
-   *
-   * @param encodedData The encoded string data as a byte array.
-   * @param numBits The number of bits used for encoding.
-   * @return The decoded original string.
-   */
-  public String decode(byte[] encodedData, int numBits) {
-    if (encodedData.length == 0) {
-      return "";
-    }
-    // The very first byte signifies the encoding used
-    Encoding chosenEncoding = Encoding.fromInt(encodedData[0] & 0xFF);
-    // Extract actual data, skipping the first byte (encoding flag)
-    encodedData = Arrays.copyOfRange(encodedData, 1, encodedData.length);
-    return decode(encodedData, chosenEncoding, numBits);
   }
 
   /**

java/fury-core/src/main/java/org/apache/fury/meta/MetaStringEncoder.java
@@ -21,6 +21,7 @@ package org.apache.fury.meta;
 
 import java.nio.charset.StandardCharsets;
 import org.apache.fury.meta.MetaString.Encoding;
+import org.apache.fury.util.Preconditions;
 
 /** Encodes plain text strings into MetaString objects with specified encoding mechanisms. */
 public class MetaStringEncoder {
@@ -48,7 +49,7 @@ public class MetaStringEncoder {
   public MetaString encode(String input) {
     if (input.isEmpty()) {
       return new MetaString(
-          input, Encoding.LOWER_SPECIAL, specialChar1, specialChar2, new byte[0], 0);
+          input, Encoding.LOWER_SPECIAL, specialChar1, specialChar2, new byte[0], 0, 0);
     }
     Encoding encoding = computeEncoding(input);
     return encode(input, encoding);
@@ -62,15 +63,23 @@ public class MetaStringEncoder {
    * @return A MetaString object representing the encoded string.
    */
   public MetaString encode(String input, Encoding encoding) {
+    Preconditions.checkArgument(
+        input.length() < Short.MAX_VALUE, "Long meta string than 32767 is not allowed");
     if (input.isEmpty()) {
       return new MetaString(
-          input, Encoding.LOWER_SPECIAL, specialChar1, specialChar2, new byte[0], 0);
+          input, Encoding.LOWER_SPECIAL, specialChar1, specialChar2, new byte[0], 0, 0);
     }
     int length = input.length();
     switch (encoding) {
       case LOWER_SPECIAL:
         return new MetaString(
-            input, encoding, specialChar1, specialChar2, encodeLowerSpecial(input), length * 5);
+            input,
+            encoding,
+            specialChar1,
+            specialChar2,
+            encodeLowerSpecial(input),
+            length,
+            length * 5);
       case LOWER_UPPER_DIGIT_SPECIAL:
         return new MetaString(
             input,
@@ -78,6 +87,7 @@ public class MetaStringEncoder {
             specialChar1,
             specialChar2,
             encodeLowerUpperDigitSpecial(input),
+            length,
             length * 6);
       case FIRST_TO_LOWER_SPECIAL:
         return new MetaString(
@@ -86,6 +96,7 @@ public class MetaStringEncoder {
             specialChar1,
             specialChar2,
             encodeFirstToLowerSpecial(input),
+            length,
             length * 5);
       case ALL_TO_LOWER_SPECIAL:
         char[] chars = input.toCharArray();
@@ -96,11 +107,12 @@ public class MetaStringEncoder {
             specialChar1,
             specialChar2,
             encodeAllToLowerSpecial(chars, upperCount),
+            length,
             (upperCount + length) * 5);
       default:
         byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
         return new MetaString(
-            input, Encoding.UTF_8, specialChar1, specialChar2, bytes, bytes.length * 8);
+            input, Encoding.UTF_8, specialChar1, specialChar2, bytes, bytes.length * 8, 0);
     }
   }
 

java/fury-core/src/main/java/org/apache/fury/resolver/ClassInfo.java
@@ -20,6 +20,8 @@
 package org.apache.fury.resolver;
 
 import org.apache.fury.config.Language;
+import org.apache.fury.meta.MetaString.Encoding;
+import org.apache.fury.meta.MetaStringEncoder;
 import org.apache.fury.serializer.Serializer;
 import org.apache.fury.util.Preconditions;
 import org.apache.fury.util.ReflectionUtils;
@@ -73,7 +75,9 @@ public class ClassInfo {
     this.serializer = serializer;
     MetaStringResolver metaStringResolver = classResolver.getMetaStringResolver();
     if (cls != null && classResolver.getFury().getLanguage() != Language.JAVA) {
-      this.fullClassNameBytes = metaStringResolver.getOrCreateMetaStringBytes(cls.getName());
+      this.fullClassNameBytes =
+          metaStringResolver.getOrCreateMetaStringBytes(
+              new MetaStringEncoder('.', '_').encode(cls.getName(), Encoding.UTF_8));
     } else {
       this.fullClassNameBytes = null;
     }
@@ -81,16 +85,21 @@ public class ClassInfo {
         && (classId == ClassResolver.NO_CLASS_ID || classId == ClassResolver.REPLACE_STUB_ID)) {
       // REPLACE_STUB_ID for write replace class in `ClassSerializer`.
       String packageName = ReflectionUtils.getPackage(cls);
-      this.packageNameBytes = metaStringResolver.getOrCreateMetaStringBytes(packageName);
+      this.packageNameBytes =
+          metaStringResolver.getOrCreateMetaStringBytes(
+              new MetaStringEncoder('.', '_').encode(packageName));
       this.classNameBytes =
           metaStringResolver.getOrCreateMetaStringBytes(
-              ReflectionUtils.getClassNameWithoutPackage(cls));
+              new MetaStringEncoder('_', '$')
+                  .encode(ReflectionUtils.getClassNameWithoutPackage(cls)));
     } else {
       this.packageNameBytes = null;
       this.classNameBytes = null;
     }
     if (tag != null) {
-      this.typeTagBytes = metaStringResolver.getOrCreateMetaStringBytes(tag);
+      this.typeTagBytes =
+          metaStringResolver.getOrCreateMetaStringBytes(
+              new MetaStringEncoder('.', '_').encode(tag, Encoding.UTF_8));
     } else {
       this.typeTagBytes = null;
     }

java/fury-core/src/main/java/org/apache/fury/resolver/ClassResolver.java
@@ -36,7 +36,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -1585,8 +1584,8 @@ public class ClassResolver {
         new ClassNameBytes(packageBytes.hashCode, simpleClassNameBytes.hashCode);
     Class<?> cls = compositeClassNameBytes2Class.get(classNameBytes);
     if (cls == null) {
-      String packageName = new String(packageBytes.bytes, StandardCharsets.UTF_8);
-      String className = new String(simpleClassNameBytes.bytes, StandardCharsets.UTF_8);
+      String packageName = packageBytes.decode('.', '_');
+      String className = simpleClassNameBytes.decode('.', '$');
       String entireClassName;
       if (StringUtils.isBlank(packageName)) {
         entireClassName = className;
@@ -1612,7 +1611,7 @@ public class ClassResolver {
     Class<?> cls = classNameBytes2Class.get(byteString);
     if (cls == null) {
       Preconditions.checkNotNull(byteString);
-      String className = new String(byteString.bytes, StandardCharsets.UTF_8);
+      String className = byteString.decode('.', '_');
       cls = loadClass(className);
       classNameBytes2Class.put(byteString, cls);
     }

java/fury-core/src/main/java/org/apache/fury/resolver/MetaStringBytes.java
@@ -19,14 +19,15 @@
 
 package org.apache.fury.resolver;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import org.apache.fury.annotation.Internal;
+import org.apache.fury.meta.MetaString;
+import org.apache.fury.meta.MetaStringDecoder;
 import org.apache.fury.util.MurmurHash3;
-import org.apache.fury.util.Preconditions;
 
 @Internal
 final class MetaStringBytes {
+  static final int STRIP_LAST_CHAR = 0b1000;
   static final short DEFAULT_DYNAMIC_WRITE_STRING_ID = -1;
 
   final byte[] bytes;
@@ -46,18 +47,35 @@ final class MetaStringBytes {
     this.hashCode = hashCode;
   }
 
-  public MetaStringBytes(String string) {
-    byte[] classNameBytes = string.getBytes(StandardCharsets.UTF_8);
-    Preconditions.checkArgument(classNameBytes.length <= Short.MAX_VALUE);
+  public MetaStringBytes(MetaString metaString) {
+    this.bytes = metaString.getBytes();
     // Set seed to ensure hash is deterministic.
-    long hashCode =
-        MurmurHash3.murmurhash3_x64_128(classNameBytes, 0, classNameBytes.length, 47)[0];
+    long hashCode = MurmurHash3.murmurhash3_x64_128(bytes, 0, bytes.length, 47)[0];
     if (hashCode == 0) {
       // Ensure hashcode is not 0, so we can do some optimization to avoid boxing.
-      hashCode += 1;
+      hashCode += 256; // last byte is reserved for header.
     }
-    this.bytes = classNameBytes;
-    this.hashCode = hashCode;
+    hashCode &= 0xffffffffffffff00L;
+    int header = metaString.getEncoding().getValue();
+    String decoded =
+        new MetaStringDecoder(metaString.getSpecialChar1(), metaString.getSpecialChar2())
+            .decode(bytes, metaString.getEncoding(), bytes.length * 8);
+    if (decoded.length() > metaString.getString().length()) {
+      header |= STRIP_LAST_CHAR;
+    }
+    this.hashCode = hashCode | header;
+  }
+
+  public String decode(char specialChar1, char specialChar2) {
+    int header = (int) (hashCode & 0xff);
+    int encodingFlags = header & 0b111;
+    MetaString.Encoding encoding = MetaString.Encoding.values()[encodingFlags];
+    String str =
+        new MetaStringDecoder(specialChar1, specialChar2).decode(bytes, encoding, bytes.length * 8);
+    if ((header & STRIP_LAST_CHAR) != 0) {
+      str = str.substring(0, str.length() - 1);
+    }
+    return str;
   }
 
   @Override
@@ -77,14 +95,10 @@ final class MetaStringBytes {
     return hashCode == that.hashCode;
   }
 
-  public byte[] getBytes() {
-    return bytes;
-  }
-
   @Override
   public int hashCode() {
     // equals will compare 8 byte hash code.
-    return (int) hashCode;
+    return (int) (hashCode >> 1);
   }
 
   @Override

java/fury-core/src/main/java/org/apache/fury/resolver/MetaStringResolver.java
@@ -19,10 +19,10 @@
 
 package org.apache.fury.resolver;
 
-import java.nio.charset.StandardCharsets;
 import org.apache.fury.collection.LongMap;
 import org.apache.fury.collection.ObjectMap;
 import org.apache.fury.memory.MemoryBuffer;
+import org.apache.fury.meta.MetaString;
 
 /**
  * A resolver for limited string value writing. Currently, we only support classname dynamic
@@ -43,7 +43,7 @@ public final class MetaStringResolver {
   private final LongMap<MetaStringBytes> hash2MetaStringBytesMap =
       new LongMap<>(initialCapacity, furyMapLoadFactor);
   // Every enum bytes should be singleton at every fury, since we keep state in it.
-  private final ObjectMap<String, MetaStringBytes> metaString2BytesMap =
+  private final ObjectMap<MetaString, MetaStringBytes> metaString2BytesMap =
       new ObjectMap<>(initialCapacity, furyMapLoadFactor);
   private MetaStringBytes[] dynamicWrittenString = new MetaStringBytes[32];
   private MetaStringBytes[] dynamicReadStringIds = new MetaStringBytes[32];
@@ -55,27 +55,13 @@ public final class MetaStringResolver {
     dynamicReadStringId = 0;
   }
 
-  MetaStringBytes getOrCreateMetaStringBytes(String str) {
+  MetaStringBytes getOrCreateMetaStringBytes(MetaString str) {
     MetaStringBytes metaStringBytes = metaString2BytesMap.get(str);
     if (metaStringBytes == null) {
       metaStringBytes = new MetaStringBytes(str);
       metaString2BytesMap.put(str, metaStringBytes);
     }
     return metaStringBytes;
-  }
-
-  public void writeMetaString(MemoryBuffer buffer, String str) {
-    writeMetaStringBytes(buffer, getOrCreateMetaStringBytes(str));
-  }
-
-  public String readMetaString(MemoryBuffer buffer) {
-    MetaStringBytes byteString = readMetaStringBytes(buffer);
-    String str = metaStringBytes2StringMap.get(byteString);
-    if (str == null) { // TODO use org.apache.fury.resolver.ObjectMap
-      str = new String(byteString.bytes, StandardCharsets.UTF_8);
-      metaStringBytes2StringMap.put(byteString, str);
-    }
-    return str;
   }
 
   public void writeMetaStringBytesWithFlag(MemoryBuffer buffer, MetaStringBytes byteString) {
@@ -118,6 +104,17 @@ public final class MetaStringResolver {
     MetaStringBytes[] tmp = new MetaStringBytes[id * 2];
     System.arraycopy(dynamicWrittenString, 0, tmp, 0, dynamicWrittenString.length);
     return this.dynamicWrittenString = tmp;
+  }
+
+  public String readMetaString(MemoryBuffer buffer) {
+    MetaStringBytes byteString = readMetaStringBytes(buffer);
+    String str = metaStringBytes2StringMap.get(byteString);
+    if (str == null) {
+      // TODO support meta string in other languages.
+      str = byteString.decode('.', '_');
+      metaStringBytes2StringMap.put(byteString, str);
+    }
+    return str;
   }
 
   public MetaStringBytes readMetaStringBytesWithFlag(MemoryBuffer buffer, int header) {

java/fury-core/src/main/resources/META-INF/native-image/org.apache.fury/fury-core/native-image.properties
@@ -169,4 +169,8 @@ Args=--initialize-at-build-time=org.apache.fury.memory.MemoryBuffer,\
     org.apache.fury.memory.MemoryUtils,\
     org.apache.fury.type.DescriptorGrouper,\
     sun.misc.Unsafe,\
-    com.google.common.collect.Platform
+    com.google.common.collect.Platform,\
+    org.apache.fury.meta.MetaString,\
+    org.apache.fury.meta.MetaStringDecoder$1,\
+    org.apache.fury.meta.MetaStringEncoder$1,\
+    org.apache.fury.meta.MetaString$Encoding

python/pyfury/_fury.py
@@ -100,7 +100,9 @@ class MetaStringBytes:
     def __init__(self, data, hashcode=None):
         self.data = data
         self.length = len(data)
-        self.hashcode = hashcode or mmh3.hash_buffer(data, 47)[0]
+        if hashcode is None:
+            hashcode = mmh3.hash_buffer(data, 47)[0] & 0xFFFFFFFFFFFFFF00
+        self.hashcode = hashcode
         self.dynamic_write_string_id = DEFAULT_DYNAMIC_WRITE_STRING_ID
 
     def __eq__(self, other):

python/pyfury/_serialization.pyx
@@ -759,7 +759,13 @@ cdef class MetaStringBytes:
     def __init__(self, data, hashcode=None):
         self.data = data
         self.length = len(data)
-        self.hashcode = hashcode or mmh3.hash_buffer(data, 47)[0]
+        if hashcode is None:
+            hashcodes = mmh3.hash_buffer(data, 47)
+            hashcode = hashcodes[0]
+            assert isinstance(hashcode, int)
+            # FIXME: why using & 0xffffffffffffff00 overflow in cython
+            hashcode = (hashcode >> 8) << 8
+        self.hashcode = hashcode
         self.dynamic_write_string_id = DEFAULT_DYNAMIC_WRITE_STRING_ID
 
     def __eq__(self, other):

java/fury-core/src/test/java/org/apache/fury/StreamTest.java
@@ -32,12 +32,77 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.fury.io.FuryInputStream;
 import org.apache.fury.io.FuryReadableChannel;
+import org.apache.fury.io.FuryStreamReader;
 import org.apache.fury.memory.MemoryBuffer;
 import org.apache.fury.test.bean.BeanA;
 import org.apache.fury.util.ReflectionUtils;
 import org.testng.annotations.Test;
 
 public class StreamTest {
+  @Test
+  public void testBufferStream() {
+    MemoryBuffer buffer0 = MemoryBuffer.newHeapBuffer(10);
+    for (int i = 0; i < 10; i++) {
+      buffer0.writeByte(i);
+      buffer0.writeChar((char) i);
+      buffer0.writeInt16((short) i);
+      buffer0.writeInt32(i);
+      buffer0.writeInt64(i);
+      buffer0.writeFloat32(i);
+      buffer0.writeFloat64(i);
+      buffer0.writeVarInt32(i);
+      buffer0.writeVarInt32(Integer.MIN_VALUE);
+      buffer0.writeVarInt32(Integer.MAX_VALUE);
+      buffer0.writeVarUint32(i);
+      buffer0.writeVarUint32(Integer.MIN_VALUE);
+      buffer0.writeVarUint32(Integer.MAX_VALUE);
+      buffer0.writeVarInt64(i);
+      buffer0.writeVarInt64(Long.MIN_VALUE);
+      buffer0.writeVarInt64(Long.MAX_VALUE);
+      buffer0.writeVarUint64(i);
+      buffer0.writeVarUint64(Long.MIN_VALUE);
+      buffer0.writeVarUint64(Long.MAX_VALUE);
+      buffer0.writeSliInt64(i);
+      buffer0.writeSliInt64(Long.MIN_VALUE);
+      buffer0.writeSliInt64(Long.MAX_VALUE);
+    }
+    byte[] bytes = buffer0.getBytes(0, buffer0.writerIndex());
+    FuryInputStream stream =
+        FuryStreamReader.of(
+            new ByteArrayInputStream(bytes) {
+              @Override
+              public synchronized int read(byte[] b, int off, int len) {
+                buffer0.readBytes(b, off, 1);
+                return 1;
+              }
+            });
+    MemoryBuffer buffer = MemoryBuffer.fromByteArray(bytes, 0, 0, stream);
+    for (int i = 0; i < 10; i++) {
+      assertEquals(buffer.readByte(), i);
+      assertEquals(buffer.readChar(), i);
+      assertEquals(buffer.readInt16(), i);
+      assertEquals(buffer.readInt32(), i);
+      assertEquals(buffer.readInt64(), i);
+      assertEquals(buffer.readFloat32(), i);
+      assertEquals(buffer.readFloat64(), i);
+      assertEquals(buffer.readVarInt32(), i);
+      assertEquals(buffer.readVarInt32(), Integer.MIN_VALUE);
+      assertEquals(buffer.readVarInt32(), Integer.MAX_VALUE);
+      assertEquals(buffer.readVarUint32(), i);
+      assertEquals(buffer.readVarUint32(), Integer.MIN_VALUE);
+      assertEquals(buffer.readVarUint32(), Integer.MAX_VALUE);
+      assertEquals(buffer.readVarInt64(), i);
+      assertEquals(buffer.readVarInt64(), Long.MIN_VALUE);
+      assertEquals(buffer.readVarInt64(), Long.MAX_VALUE);
+      assertEquals(buffer.readVarUint64(), i);
+      assertEquals(buffer.readVarUint64(), Long.MIN_VALUE);
+      assertEquals(buffer.readVarUint64(), Long.MAX_VALUE);
+      assertEquals(buffer.readSliInt64(), i);
+      assertEquals(buffer.readSliInt64(), Long.MIN_VALUE);
+      assertEquals(buffer.readSliInt64(), Long.MAX_VALUE);
+    }
+  }
+
   @Test
   public void testBufferReset() {
     Fury fury = Fury.builder().withRefTracking(true).requireClassRegistration(false).build();

java/fury-core/src/test/java/org/apache/fury/resolver/MetaStringResolverTest.java
@@ -24,6 +24,8 @@ import static org.testng.Assert.assertTrue;
 
 import org.apache.fury.memory.MemoryBuffer;
 import org.apache.fury.memory.MemoryUtils;
+import org.apache.fury.meta.MetaString;
+import org.apache.fury.meta.MetaStringEncoder;
 import org.apache.fury.util.StringUtils;
 import org.testng.annotations.Test;
 
@@ -35,7 +37,9 @@ public class MetaStringResolverTest {
     String str = StringUtils.random(128, 0);
     MetaStringResolver stringResolver = new MetaStringResolver();
     for (int i = 0; i < 128; i++) {
-      stringResolver.writeMetaString(buffer, str);
+      MetaString metaString = new MetaStringEncoder('.', '_').encode(str);
+      stringResolver.writeMetaStringBytes(
+          buffer, stringResolver.getOrCreateMetaStringBytes(metaString));
     }
     for (int i = 0; i < 128; i++) {
       String metaString = stringResolver.readMetaString(buffer);