apache/fury

commit 63bbe45d863eed0d56e3d08c35e7e007463b7420

feat(java): type meta encoding for java (#1556)
## What does this PR do?

This PR implements type meta encoding for java proposed in #1240 .

The type meta encoding in xlang spec proposed in #1413 will be finished
in another PR based on this PR.

The spec has been updated too:

type meta header
```
|      8 bytes meta header      | meta size |   variable bytes   |  variable bytes   | variable bytes |
+-------------------------------+-----------|--------------------+-------------------+----------------+
| 7 bytes hash + 1 bytes header | 1~2 bytes | current class meta | parent class meta |      ...       |
```

And the encoding for packge/class/field name has been updated to:
```
- Package name encoding(omitted when class is registered):
    - encoding algorithm: `UTF8/ALL_TO_LOWER_SPECIAL/LOWER_UPPER_DIGIT_SPECIAL`
    - Header: `6 bits size | 2 bits encoding flags`. The `6 bits size: 0~63`  will be used to indicate size `0~62`,
      the value `63` the size need more byte to read, the encoding will encode `size - 62` as a varint next.
- Class name encoding(omitted when class is registered):
    - encoding algorithm: `UTF8/LOWER_UPPER_DIGIT_SPECIAL/FIRST_TO_LOWER_SPECIAL/ALL_TO_LOWER_SPECIAL`
    - header: `6 bits size | 2 bits encoding flags`. The `6 bits size: 0~63`  will be used to indicate size `1~64`,
      the value `63` the size need more byte to read, the encoding will encode `size - 63` as a varint next.
- Field info:
    - header(8
      bits): `3 bits size + 2 bits field name encoding + polymorphism flag + nullability flag + ref tracking flag`.
      Users can use annotation to provide those info.
        - 2 bits field name encoding:
            - encoding: `UTF8/ALL_TO_LOWER_SPECIAL/LOWER_UPPER_DIGIT_SPECIAL/TAG_ID`
            - If tag id is used, i.e. field name is written by an unsigned varint tag id. 2 bits encoding will be `11`.
        - size of field name:

java/fury-core/src/main/java/org/apache/fury/builder/ObjectCodecBuilder.java
@@ -56,6 +56,7 @@ import org.apache.fury.codegen.Expression.ReplaceStub;
 import org.apache.fury.codegen.Expression.StaticInvoke;
 import org.apache.fury.codegen.ExpressionVisitor;
 import org.apache.fury.memory.Platform;
+import org.apache.fury.meta.ClassDef;
 import org.apache.fury.reflect.TypeRef;
 import org.apache.fury.serializer.ObjectSerializer;
 import org.apache.fury.serializer.PrimitiveSerializers.LongSerializer;
@@ -87,13 +88,23 @@ public class ObjectCodecBuilder extends BaseObjectCodecBuilder {
 
   public ObjectCodecBuilder(Class<?> beanClass, Fury fury) {
     super(TypeRef.of(beanClass), fury, Generated.GeneratedObjectSerializer.class);
-    Collection<Descriptor> descriptors =
-        classResolver.getAllDescriptorsMap(beanClass, true).values();
+    Collection<Descriptor> descriptors;
+    boolean shareMeta = fury.getConfig().shareMetaContext();
+    if (shareMeta) {
+      ClassDef classDef = classResolver.getClassDef(beanClass, true);
+      descriptors = classDef.getDescriptors(classResolver, beanClass);
+    } else {
+      descriptors = fury.getClassResolver().getAllDescriptorsMap(beanClass, true).values();
+    }
     classVersionHash =
         new Literal(ObjectSerializer.computeVersionHash(descriptors), PRIMITIVE_INT_TYPE);
     DescriptorGrouper grouper =
         DescriptorGrouper.createDescriptorGrouper(
-            descriptors, false, fury.compressInt(), fury.compressLong());
+            fury.getClassResolver()::isMonomorphic,
+            descriptors,
+            false,
+            fury.compressInt(),
+            fury.compressLong());
     objectCodecOptimizer =
         new ObjectCodecOptimizer(beanClass, grouper, !fury.isBasicTypesRefIgnored(), ctx);
     if (isRecord) {

commit 48361cb7b7c13e703f0d5e1a10d5d104c9cd78a2

fix(java): fix getClassDef thead safety (#1597)
## What does this PR do?

Fix ci failuure introduced in #1556 :

![image](https://github.com/apache/incubator-fury/assets/12445254/5977b4ca-07b9-456b-82d8-a2779a08d01f)


## Related issues

<!--
Is there any related issue? Please attach here.

- #xxxx0
- #xxxx1
- #xxxx2
-->


## Does this PR introduce any user-facing change?

<!--
If any user-facing interface changes, please [open an
issue](https://github.com/apache/incubator-fury/issues/new/choose)
describing the need to do so and update the document if necessary.
-->

- [ ] Does this PR introduce any public API change?
- [ ] Does this PR introduce any binary protocol compatibility change?


## Benchmark

<!--
When the PR has an impact on performance (if you don't know whether the
PR will have an impact on performance, you can submit the PR first, and
if it will have impact on performance, the code reviewer will explain
it), be sure to attach a benchmark data here.
-->

java/fury-core/src/main/java/org/apache/fury/builder/BaseObjectCodecBuilder.java
@@ -189,7 +189,7 @@ public abstract class BaseObjectCodecBuilder extends CodecBuilder {
 
   protected abstract String codecSuffix();
 
-  <T> T visitFury(Function<Fury, T> function) {
+  protected <T> T visitFury(Function<Fury, T> function) {
     return fury.getJITContext().asyncVisitFury(function);
   }
 
@@ -441,8 +441,7 @@ public abstract class BaseObjectCodecBuilder extends CodecBuilder {
                   classInfo,
                   inlineInvoke(classResolverRef, "getClassInfo", classInfoTypeRef, clsExpr))));
     }
-    writeClassAndObject.add(
-        fury.getClassResolver().writeClassExpr(classResolverRef, buffer, classInfo));
+    writeClassAndObject.add(classResolver.writeClassExpr(classResolverRef, buffer, classInfo));
     writeClassAndObject.add(
         new Invoke(
             inlineInvoke(classInfo, "getSerializer", SERIALIZER_TYPE),
@@ -659,8 +658,7 @@ public abstract class BaseObjectCodecBuilder extends CodecBuilder {
                 new Assign(
                     classInfo,
                     inlineInvoke(classResolverRef, "getClassInfo", classInfoTypeRef, clsExpr))));
-        writeClassAction.add(
-            fury.getClassResolver().writeClassExpr(classResolverRef, buffer, classInfo));
+        writeClassAction.add(classResolver.writeClassExpr(classResolverRef, buffer, classInfo));
         serializer = new Invoke(classInfo, "getSerializer", "serializer", SERIALIZER_TYPE, false);
         serializer = new Cast(serializer, TypeRef.of(AbstractCollectionSerializer.class));
         writeClassAction.add(serializer, new Return(serializer));
@@ -965,8 +963,7 @@ public abstract class BaseObjectCodecBuilder extends CodecBuilder {
                     classInfo,
                     inlineInvoke(classResolverRef, "getClassInfo", classInfoTypeRef, clsExpr))));
         // Note: writeClassExpr is thread safe.
-        writeClassAction.add(
-            fury.getClassResolver().writeClassExpr(classResolverRef, buffer, classInfo));
+        writeClassAction.add(classResolver.writeClassExpr(classResolverRef, buffer, classInfo));
         serializer = new Invoke(classInfo, "getSerializer", "serializer", SERIALIZER_TYPE, false);
         serializer = new Cast(serializer, TypeRef.of(AbstractMapSerializer.class));
         writeClassAction.add(serializer, new Return(serializer));

java/fury-core/src/main/java/org/apache/fury/builder/CompatibleCodecBuilder.java
@@ -41,7 +41,6 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.apache.fury.Fury;
 import org.apache.fury.builder.Generated.GeneratedCompatibleSerializer;
 import org.apache.fury.codegen.CodegenContext;
@@ -97,10 +96,6 @@ public class CompatibleCodecBuilder extends BaseObjectCodecBuilder {
     super(beanType, fury, superSerializerClass);
     this.fieldResolver = fieldResolver;
     if (isRecord) {
-      List<String> fieldNames =
-          fieldResolver.getAllFieldsList().stream()
-              .map(FieldResolver.FieldInfo::getName)
-              .collect(Collectors.toList());
       recordReversedMapping = RecordUtils.buildFieldToComponentMapping(beanClass);
     }
     ctx.reserveName(FIELD_RESOLVER_NAME);
@@ -879,7 +874,7 @@ public class CompatibleCodecBuilder extends BaseObjectCodecBuilder {
     Preconditions.checkArgument(ReflectionUtils.isMonomorphic(cls));
     ClassInfo classInfo = visitFury(f -> f.getClassResolver().getClassInfo(cls, false));
     if (classInfo != null && classInfo.getClassId() != ClassResolver.NO_CLASS_ID) {
-      return fury.getClassResolver().writeClassExpr(buffer, classInfo.getClassId());
+      return classResolver.writeClassExpr(buffer, classInfo.getClassId());
     }
     Expression classInfoExpr = getFinalClassInfo(cls);
     return new Invoke(classResolverRef, "writeClass", buffer, classInfoExpr);
@@ -889,7 +884,7 @@ public class CompatibleCodecBuilder extends BaseObjectCodecBuilder {
     Preconditions.checkArgument(ReflectionUtils.isMonomorphic(cls));
     ClassInfo classInfo = visitFury(f -> f.getClassResolver().getClassInfo(cls, false));
     if (classInfo != null && classInfo.getClassId() != ClassResolver.NO_CLASS_ID) {
-      return fury.getClassResolver().skipRegisteredClassExpr(buffer);
+      return classResolver.skipRegisteredClassExpr(buffer);
     }
     // read `ClassInfo` is not used, set `inlineReadClassInfo` false,
     // to avoid read doesn't happen in generated code.

java/fury-core/src/main/java/org/apache/fury/builder/ObjectCodecBuilder.java
@@ -56,7 +56,6 @@ import org.apache.fury.codegen.Expression.ReplaceStub;
 import org.apache.fury.codegen.Expression.StaticInvoke;
 import org.apache.fury.codegen.ExpressionVisitor;
 import org.apache.fury.memory.Platform;
-import org.apache.fury.meta.ClassDef;
 import org.apache.fury.reflect.TypeRef;
 import org.apache.fury.serializer.ObjectSerializer;
 import org.apache.fury.serializer.PrimitiveSerializers.LongSerializer;
@@ -91,8 +90,12 @@ public class ObjectCodecBuilder extends BaseObjectCodecBuilder {
     Collection<Descriptor> descriptors;
     boolean shareMeta = fury.getConfig().shareMetaContext();
     if (shareMeta) {
-      ClassDef classDef = classResolver.getClassDef(beanClass, true);
-      descriptors = classDef.getDescriptors(classResolver, beanClass);
+      descriptors =
+          visitFury(
+              f ->
+                  f.getClassResolver()
+                      .getClassDef(beanClass, true)
+                      .getDescriptors(classResolver, beanClass));
     } else {
       descriptors = fury.getClassResolver().getAllDescriptorsMap(beanClass, true).values();
     }

java/fury-core/src/main/java/org/apache/fury/meta/ClassDef.java
@@ -203,6 +203,14 @@ public class ClassDef implements Serializable {
     return ClassDefDecoder.decodeClassDef(classResolver, buffer, header);
   }
 
+  /**
+   * Consolidate fields of <code>classDef</code> with <code>cls</code>. If some field exists in
+   * <code>cls</code> but not in <code>classDef</code>, it won't be returned in final collection. If
+   * some field exists in <code>classDef</code> but not in <code> cls</code>, it will be added to
+   * final collection.
+   *
+   * @param cls class load in current process.
+   */
   public List<Descriptor> getDescriptors(ClassResolver resolver, Class<?> cls) {
     if (descriptors == null) {
       SortedMap<Field, Descriptor> allDescriptorsMap = resolver.getAllDescriptorsMap(cls, true);

java/fury-core/src/main/java/org/apache/fury/resolver/ClassResolver.java
@@ -510,7 +510,7 @@ public class ClassResolver {
   /**
    * Mark non-inner registered final types as non-final to write class def for those types. Note if
    * a class is registered but not an inner class with inner serializer, it will still be taken as
-   * non-final to write class def, so that it can be deserialized by the peer still..
+   * non-final to write class def, so that it can be deserialized by the peer still.
    */
   public boolean isMonomorphic(Class<?> clz) {
     if (fury.getConfig().shareMetaContext()) {

java/fury-core/src/main/java/org/apache/fury/serializer/MetaSharedSerializer.java
@@ -287,15 +287,6 @@ public class MetaSharedSerializer<T> extends Serializer<T> {
     }
   }
 
-  /**
-   * Consolidate fields of <code>classDef</code> with <code>cls</code>. If some field exists in
-   * <code>cls</code> but not in <code>classDef</code>, it won't be returned in final collection. If
-   * some field exists in <code>classDef</code> but not in <code> cls</code>, it will be added to
-   * final collection.
-   *
-   * @param cls class load in current process.
-   * @param classDef class definition sent from peer.
-   */
   public static Collection<Descriptor> consolidateFields(
       ClassResolver classResolver, Class<?> cls, ClassDef classDef) {
     return classDef.getDescriptors(classResolver, cls);