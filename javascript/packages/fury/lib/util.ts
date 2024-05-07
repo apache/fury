5cccfe5376c10ff9b1591681284d9325fb1e06ed
野声 <lijiacheng.ljc@antgroup.com>
2024-05-07 15:23:56 +0800

    feat: add data to description util

--- javascript/packages/fury/lib/util.ts
+++ javascript/packages/fury/lib/util.ts
@@ -17,6 +17,9 @@
  * under the License.
  */
 
+import { ObjectTypeDescription, Type, TypeDescription } from "./description";
+import { InternalSerializerType } from "./type";
+
 export const isNodeEnv: boolean
   = typeof process !== "undefined"
   && process.versions != null
@@ -24,3 +27,93 @@ export const isNodeEnv: boolean
   && process.versions.node != null;
 
 export const hasBuffer = isNodeEnv && typeof Buffer !== "undefined";
+
+export function isUint8Array(obj: any): obj is Uint8Array {
+  return obj instanceof Uint8Array || Object.prototype.toString.call(obj) === "[object Uint8Array]";
+}
+
+export const data2Description = (
+  data: any,
+  tag: string,
+): TypeDescription | null => {
+  if (data === null || data === undefined) {
+    return null;
+  }
+  if (Array.isArray(data)) {
+    const item = data2Description(data[0], tag);
+    if (!item) {
+      throw new Error("empty array can't convert");
+    }
+    return {
+      ...Type.array(item),
+      label: "array",
+    };
+  }
+  if (data instanceof Date) {
+    return {
+      type: InternalSerializerType.DURATION,
+      label: "timestamp",
+    };
+  }
+  if (typeof data === "string") {
+    return {
+      type: InternalSerializerType.STRING,
+      label: "string",
+    };
+  }
+  if (data instanceof Set) {
+    return {
+      ...Type.set(data2Description([...data.values()][0], tag)!),
+      label: "set",
+    };
+  }
+  if (data instanceof Map) {
+    return {
+      ...Type.map(
+        data2Description([...data.keys()][0], tag)!,
+        data2Description([...data.values()][0], tag)!,
+      ),
+      label: "map",
+    };
+  }
+  if (typeof data === "boolean") {
+    return {
+      type: InternalSerializerType.BOOL,
+      label: "boolean",
+    };
+  }
+  if (typeof data === "number") {
+    if (data > Number.MAX_SAFE_INTEGER || data < Number.MIN_SAFE_INTEGER) {
+      return {
+        type: InternalSerializerType.INT64,
+        label: "int64",
+      };
+    }
+    return {
+      type: InternalSerializerType.INT32,
+      label: "int32",
+    };
+  }
+  if (typeof data === "object") {
+    if (isUint8Array(data)) {
+      return Type.binary();
+    }
+
+    return {
+      type: InternalSerializerType.OBJECT,
+      label: "object",
+      options: {
+        props: Object.fromEntries(
+          Object.entries(data)
+            .map(([key, value]) => {
+              return [key, data2Description(value, `${tag}.${key}`)];
+            })
+            .filter(([k, v]) => Boolean(v)),
+        ),
+        tag,
+      },
+    } as ObjectTypeDescription;
+  }
+
+  throw `unkonw data type ${typeof data}`;
+};
