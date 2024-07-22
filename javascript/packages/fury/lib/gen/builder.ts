48089d4a4efe7262c5be37bcef6750308285ec6d
FORCHA PEARL <forchapearl1@gmail.com>
2024-07-22 11:37:29 +0100

    revert Uint8Array changes

--- javascript/packages/fury/lib/gen/builder.ts
+++ javascript/packages/fury/lib/gen/builder.ts
@@ -205,13 +205,8 @@ class BinaryWriterBuilder {
     return `${this.holder}.uint64(${v})`;
   }
 
-  buffer(v: string | Uint8Array) { // Accepting Uint8Array as a parameter
-    if (typeof v === "string") {
+  buffer(v: string) { 
       return `${this.holder}.buffer(${v})`;
-    } else {
-      // Handle Uint8Array input as needed
-      return `${this.holder}.buffer(new Uint8Array(${v}))`;
-    }
   }
 
   float64(v: number | string) {
