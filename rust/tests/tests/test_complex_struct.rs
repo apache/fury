f5960a1b05290862ba5f10b032dc4e7b22ee9910
theweipeng <wangweipeng@apache.org>
2024-08-04 22:08:19 +0800

    revert unit test which was deleted by accident

--- rust/tests/tests/test_complex_struct.rs
+++ rust/tests/tests/test_complex_struct.rs
@@ -32,12 +32,39 @@ fn complex_struct() {
     #[derive(Fury, Debug, PartialEq, Default)]
     #[tag("example.foo")]
     struct Person {
+        c1: Vec<u8>,  // binary
+        c2: Vec<i16>, // primitive array
         animal: Vec<Animal>,
+        c3: Vec<Vec<u8>>,
+        name: String,
+        c4: HashMap<String, String>,
+        age: u16,
+        op: Option<String>,
+        op2: Option<String>,
+        date: NaiveDate,
+        time: NaiveDateTime,
+        c5: f32,
+        c6: f64,
     }
     let person: Person = Person {
+        c1: vec![1, 2, 3],
+        c2: vec![5, 6, 7],
+        c3: vec![vec![1, 2], vec![1, 3]],
         animal: vec![Animal {
             category: "Dog".to_string(),
         }],
+        c4: HashMap::from([
+            ("hello1".to_string(), "hello2".to_string()),
+            ("hello2".to_string(), "hello3".to_string()),
+        ]),
+        age: 12,
+        name: "hello".to_string(),
+        op: Some("option".to_string()),
+        op2: None,
+        date: NaiveDate::from_ymd_opt(2025, 12, 12).unwrap(),
+        time: DateTime::from_timestamp(1689912359, 0).unwrap().naive_utc(),
+        c5: 2.0,
+        c6: 4.0,
     };
     let fury = Fury::default().mode(Mode::Compatible);
     let bin: Vec<u8> = fury.serialize(&person);
