1434328f3342dc41892c17d8d70e70c3c9687be6
FORCHA PEARL <forchapearl1@gmail.com>
2024-09-24 07:25:56 +0100

    fixed lint errors

--- javascript/packages/fury/lib/gen/builder.ts
+++ javascript/packages/fury/lib/gen/builder.ts
@@ -17,142 +17,372 @@
  * under the License.
  */
 
-import { InternalSerializerType, MaxInt32 } from "../type";
 import { Scope } from "./scope";
-import { CodecBuilder } from "./builder";
-import { ObjectTypeDescription, TypeDescription } from "../description";
-import { fromString } from "../platformBuffer";
-import { CodegenRegistry } from "./router";
-import { BaseSerializerGenerator, RefState } from "./serializer";
-import SerializerResolver from "../classResolver";
-import { MetaString } from "../meta/MetaString";
-import { FieldInfo, TypeMeta } from "../meta/TypeMeta";
+import { getMeta } from "../meta";
+import { TypeDescription } from "../description";
+import Fury from "../fury";
+import { TypeMeta } from "../meta/TypeMeta";
+import { BinaryReader } from "../reader";
 
-function computeFieldHash(hash: number, id: number): number {
-  let newHash = (hash) * 31 + (id);
-  while (newHash >= MaxInt32) {
-    newHash = Math.floor(newHash / 7);
-  }
-  return newHash;
-}
+class TypeMetaBuilder {
+  constructor(private fury: string) {
 
-const computeStringHash = (str: string) => {
-  const bytes = fromString(str);
-  let hash = 17;
-  bytes.forEach((b) => {
-    hash = hash * 31 + b;
-    while (hash >= MaxInt32) {
-      hash = Math.floor(hash / 7);
-    }
-  });
-  return hash;
-};
-
-const computeStructHash = (description: TypeDescription) => {
-  let hash = 17;
-  for (const [, value] of Object.entries((<ObjectTypeDescription>description).options.props).sort()) {
-    let id = SerializerResolver.getTypeIdByInternalSerializerType(value.type);
-    if (value.type === InternalSerializerType.OBJECT) {
-      id = computeStringHash((<ObjectTypeDescription>value).options.tag);
-    }
-    hash = computeFieldHash(hash, id);
-  }
-  return hash;
-};
-
-class ObjectSerializerGenerator extends BaseSerializerGenerator {
-  description: ObjectTypeDescription;
-
-  constructor(description: TypeDescription, builder: CodecBuilder, scope: Scope) {
-    super(description, builder, scope);
-    this.description = <ObjectTypeDescription>description;
   }
 
-  writeStmt(accessor: string): string {
-    const options = this.description.options;
-    const expectHash = computeStructHash(this.description);
-    // const metaInformation = Buffer.from(computeMetaInformation(this.description));
-    const fields = Object.entries(this.description).map(([key, value]) => {
-      return new FieldInfo(key, value.type);
-    });
-    const typeMetaBinary = new Uint8Array(TypeMeta.from_fields(256, fields).to_bytes());
-    const typeMetaDeclare = this.scope.declare("typeMeta", `new Uint8Array([${typeMetaBinary.toString()}])`);
-
-    return `
-      ${this.builder.writer.int32(expectHash)};
-      
-      ${this.builder.writer.buffer(typeMetaDeclare)};
-
-      ${Object.entries(options.props).sort().map(([key, inner]) => {
-        const InnerGeneratorClass = CodegenRegistry.get(inner.type);
-        if (!InnerGeneratorClass) {
-            throw new Error(`${inner.type} generator not exists`);
-        }
-        const innerGenerator = new InnerGeneratorClass(inner, this.builder, this.scope);
-        return innerGenerator.toWriteEmbed(`${accessor}${CodecBuilder.safePropAccessor(key)}`);
-      }).join(";\n")}
-    `;
-  }
-
-  readStmt(accessor: (expr: string) => string, refState: RefState): string {
-    const options = this.description.options;
-    const expectHash = computeStructHash(this.description);
-    const result = this.scope.uniqueName("result");
-    const fields = Object.entries(options.props).map(([key, value]) => {
-      return new FieldInfo(key, value.type);
-    });
-    const binary = TypeMeta.from_fields(256, fields).to_bytes();
-    const buffer = Buffer.from(binary);
-
-    return `
-      if (${this.builder.reader.int32()} !== ${expectHash}) {
-          throw new Error("got ${this.builder.reader.int32()} validate hash failed: ${this.safeTag()}. expect ${expectHash}");
-      }
-      const ${result} = {
-        ${Object.entries(options.props).sort().map(([key]) => {
-          return `${CodecBuilder.safePropName(key)}: null`;
-        }).join(",\n")}
-      };
-
-      ${this.maybeReference(result, refState)}
-      ${this.builder.typeMeta.from_bytes(this.builder.reader.ownName())}
-      ${Object.entries(options.props).sort().map(([key, inner]) => {
-        const InnerGeneratorClass = CodegenRegistry.get(inner.type);
-        if (!InnerGeneratorClass) {
-          throw new Error(`${inner.type} generator not exists`);
-        }
-        const innerGenerator = new InnerGeneratorClass(inner, this.builder, this.scope);
-        return innerGenerator.toReadEmbed(expr => `${result}${CodecBuilder.safePropAccessor(key)} = ${expr}`);
-      }).join(";\n")}
-      ${accessor(result)}
-    `;
-  }
-
-  private safeTag() {
-    return CodecBuilder.replaceBackslashAndQuote(this.description.options.tag);
-  }
-
-  toReadEmbed(accessor: (expr: string) => string, excludeHead?: boolean, refState?: RefState): string {
-    const name = this.scope.declare(
-      "tag_ser",
-      `fury.classResolver.getSerializerByTag("${this.safeTag()}")`
-    );
-    if (!excludeHead) {
-      return accessor(`${name}.read()`);
-    }
-    return accessor(`${name}.readInner(${refState!.toConditionExpr()})`);
-  }
-
-  toWriteEmbed(accessor: string, excludeHead?: boolean): string {
-    const name = this.scope.declare(
-      "tag_ser",
-      `fury.classResolver.getSerializerByTag("${this.safeTag()}")`
-    );
-    if (!excludeHead) {
-      return `${name}.write(${accessor})`;
-    }
-    return `${name}.writeInner(${accessor})`;
+  from_bytes(reader: string) {
+    return `${this.fury}.typeMeta.from_bytes(${reader})`;
   }
 }
 
-CodegenRegistry.register(InternalSerializerType.OBJECT, ObjectSerializerGenerator);
+export class BinaryReaderBuilder {
+  constructor(private holder: string) {
+
+  }
+
+  ownName() {
+    return this.holder;
+  }
+
+  getCursor() {
+    return `${this.holder}.getCursor()`;
+  }
+
+  setCursor(v: number | string) {
+    return `${this.holder}.setCursor(${v})`;
+  }
+
+  varInt32() {
+    return `${this.holder}.varInt32()`;
+  }
+
+  varInt64() {
+    return `${this.holder}.varInt64()`;
+  }
+
+  varUInt32() {
+    return `${this.holder}.varUInt32()`;
+  }
+
+  varUInt64() {
+    return `${this.holder}.varUInt64()`;
+  }
+
+  int8() {
+    return `${this.holder}.int8()`;
+  }
+
+  buffer(len: string | number) {
+    return `${this.holder}.buffer(${len})`;
+  }
+
+  bufferRef() {
+    return `${this.holder}.bufferRef()`;
+  }
+
+  uint8() {
+    return `${this.holder}.uint8()`;
+  }
+
+  stringUtf8At() {
+    return `${this.holder}.stringUtf8At()`;
+  }
+
+  stringUtf8() {
+    return `${this.holder}.stringUtf8()`;
+  }
+
+  stringLatin1() {
+    return `${this.holder}.stringLatin1()`;
+  }
+
+  stringOfVarUInt32() {
+    return `${this.holder}.stringOfVarUInt32()`;
+  }
+
+  float64() {
+    return `${this.holder}.float64()`;
+  }
+
+  float32() {
+    return `${this.holder}.float32()`;
+  }
+
+  float16() {
+    return `${this.holder}.float16()`;
+  }
+
+  uint16() {
+    return `${this.holder}.uint16()`;
+  }
+
+  int16() {
+    return `${this.holder}.int16()`;
+  }
+
+  uint64() {
+    return `${this.holder}.uint64()`;
+  }
+
+  skip(v: number) {
+    return `${this.holder}.skip(${v})`;
+  }
+
+  int64() {
+    return `${this.holder}.int64()`;
+  }
+
+  sliInt64() {
+    return `${this.holder}.sliInt64()`;
+  }
+
+  uint32() {
+    return `${this.holder}.uint32()`;
+  }
+
+  int32() {
+    return `${this.holder}.int32()`;
+  }
+}
+
+class BinaryWriterBuilder {
+  constructor(private holder: string) {
+
+  }
+
+  ownName() {
+    return this.holder;
+  }
+
+  skip(v: number | string) {
+    return `${this.holder}.skip(${v})`;
+  }
+
+  getByteLen() {
+    return `${this.holder}.getByteLen()`;
+  }
+
+  getReserved() {
+    return `${this.holder}.getReserved()`;
+  }
+
+  reserve(v: number | string) {
+    return `${this.holder}.reserve(${v})`;
+  }
+
+  uint16(v: number | string) {
+    return `${this.holder}.uint16(${v})`;
+  }
+
+  int8(v: number | string) {
+    return `${this.holder}.int8(${v})`;
+  }
+
+  int24(v: number | string) {
+    return `${this.holder}.int24(${v})`;
+  }
+
+  uint8(v: number | string) {
+    return `${this.holder}.uint8(${v})`;
+  }
+
+  int16(v: number | string) {
+    return `${this.holder}.int16(${v})`;
+  }
+
+  varInt32(v: number | string) {
+    return `${this.holder}.varInt32(${v})`;
+  }
+
+  varUInt32(v: number | string) {
+    return `${this.holder}.varUInt32(${v})`;
+  }
+
+  varUInt64(v: number | string) {
+    return `${this.holder}.varUInt64(${v})`;
+  }
+
+  varInt64(v: number | string) {
+    return `${this.holder}.varInt64(${v})`;
+  }
+
+  stringOfVarUInt32(str: string) {
+    return `${this.holder}.stringOfVarUInt32(${str})`;
+  }
+
+  bufferWithoutMemCheck(v: string) {
+    return `${this.holder}.bufferWithoutMemCheck(${v})`;
+  }
+
+  uint64(v: number | string) {
+    return `${this.holder}.uint64(${v})`;
+  }
+
+  buffer(v: string) { // Accepting Uint8Array as a parameter
+    return `${this.holder}.buffer(${v})`;
+  }
+
+  float64(v: number | string) {
+    return `${this.holder}.float64(${v})`;
+  }
+
+  float32(v: number | string) {
+    return `${this.holder}.float32(${v})`;
+  }
+
+  int64(v: number | string) {
+    return `${this.holder}.int64(${v})`;
+  }
+
+  sliInt64(v: number | string) {
+    return `${this.holder}.sliInt64(${v})`;
+  }
+
+  uint32(v: number | string) {
+    return `${this.holder}.uint32(${v})`;
+  }
+
+  int32(v: number | string) {
+    return `${this.holder}.int32(${v})`;
+  }
+
+  getCursor() {
+    return `${this.holder}.getCursor()`;
+  }
+
+  setUint32Position(offset: number | string, v: number | string) {
+    return `${this.holder}.setUint32Position(${offset}, ${v})`;
+  }
+
+  setUint8Position(offset: number | string, v: number | string) {
+    return `${this.holder}.setUint8Position(${offset}, ${v})`;
+  }
+
+  setUint16Position(offset: number | string, v: number | string) {
+    return `${this.holder}.setUint16Position(${offset}, ${v})`;
+  }
+}
+
+class ReferenceResolverBuilder {
+  constructor(private holder: string) {
+
+  }
+
+  ownName() {
+    return this.holder;
+  }
+
+  getReadObject(id: string | number) {
+    return `${this.holder}.getReadObject(${id})`;
+  }
+
+  reference(obj: string) {
+    return `${this.holder}.reference(${obj})`;
+  }
+
+  writeRef(obj: string) {
+    return `${this.holder}.writeRef(${obj})`;
+  }
+
+  existsWriteObject(obj: string) {
+    return `${this.holder}.existsWriteObject(${obj})`;
+  }
+}
+
+class ClassResolverBuilder {
+  constructor(private holder: string) {
+
+  }
+
+  ownName() {
+    return this.holder;
+  }
+
+  getSerializerById(id: string | number) {
+    return `${this.holder}.getSerializerById(${id})`;
+  }
+
+  getSerializerByTag(tag: string) {
+    return `${this.holder}.getSerializerByTag(${tag})`;
+  }
+
+  createTagWriter(tag: string) {
+    return `${this.holder}.createTagWriter("${tag}")`;
+  }
+
+  readTag(binaryReader: string) {
+    return `${this.holder}.readTag(${binaryReader})`;
+  }
+
+  getSerializerByData(v: string) {
+    return `${this.holder}.readTag(${v})`;
+  }
+}
+
+export class CodecBuilder {
+  reader: BinaryReaderBuilder;
+  writer: BinaryWriterBuilder;
+  typeMeta: TypeMetaBuilder; // Use the TypeMetaWrapper
+  referenceResolver: ReferenceResolverBuilder;
+  classResolver: ClassResolverBuilder;
+
+  constructor(scope: Scope, public fury: Fury) {
+    const br = scope.declareByName("br", "fury.binaryReader");
+    const bw = scope.declareByName("bw", "fury.binaryWriter");
+    const cr = scope.declareByName("cr", "fury.classResolver");
+    const rr = scope.declareByName("rr", "fury.referenceResolver");
+    this.reader = new BinaryReaderBuilder(br);
+    this.writer = new BinaryWriterBuilder(bw);
+    this.classResolver = new ClassResolverBuilder(cr);
+    this.referenceResolver = new ReferenceResolverBuilder(rr);
+    this.typeMeta = new TypeMetaBuilder("fury"); // Initialize the TypeMetaWrapper
+  }
+
+  furyName() {
+    return "fury";
+  }
+
+  meta(description: TypeDescription) {
+    return getMeta(description, this.fury);
+  }
+
+  config() {
+    return this.fury.config;
+  }
+
+  static isReserved(key: string) {
+    return /^(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|yield|delete|export|import|public|return|static|switch|typeof|default|extends|finally|package|private|continue|debugger|function|arguments|interface|protected|implements|instanceof)$/.test(key);
+  }
+
+  static isDotPropAccessor(prop: string) {
+    return /^[a-zA-Z_$][0-9a-zA-Z_$]*$/.test(prop);
+  }
+
+  static replaceBackslashAndQuote(v: string) {
+    return v.replace(/\\/g, "\\\\").replace(/"/g, "\\\"");
+  }
+
+  static safeString(target: string) {
+    if (!CodecBuilder.isDotPropAccessor(target) || CodecBuilder.isReserved(target)) {
+      return `"${CodecBuilder.replaceBackslashAndQuote(target)}"`;
+    }
+    return `"${target}"`;
+  }
+
+  static safePropAccessor(prop: string) {
+    if (!CodecBuilder.isDotPropAccessor(prop) || CodecBuilder.isReserved(prop)) {
+      return `["${CodecBuilder.replaceBackslashAndQuote(prop)}"]`;
+    }
+    return `.${prop}`;
+  }
+
+  static safePropName(prop: string) {
+    if (!CodecBuilder.isDotPropAccessor(prop) || CodecBuilder.isReserved(prop)) {
+      return `["${CodecBuilder.replaceBackslashAndQuote(prop)}"]`;
+    }
+    return prop;
+  }
+
+  getExternal(key: string) {
+    return `external.${key}`;
+  }
+}
