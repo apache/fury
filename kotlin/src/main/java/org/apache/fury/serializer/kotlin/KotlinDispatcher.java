65a510624e3c86d8e5719525ce852d1b79a3b165
Yi Wen Wong <wongyw86@gmail.com>
2024-10-03 07:11:51 -0700

    Basic Kotlin Dispatcher skeleton.

--- kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinDispatcher.java
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinDispatcher.java
@@ -1,4 +1,16 @@
 package org.apache.fury.serializer.kotlin;
 
-public class KotlinDispatcher {
+import org.apache.fury.Fury;
+import org.apache.fury.serializer.Serializer;
+import org.apache.fury.serializer.SerializerFactory;
+
+/**
+ * Serializer dispatcher for kotlin types.
+ */
+@SuppressWarnings({"rawtypes"})
+public class KotlinDispatcher implements SerializerFactory {
+    @Override
+    public Serializer createSerializer(Fury fury, Class<?> aClass) {
+        return null;
+    }
 }
