98d52f76d76d1475570287b692d08bdb7a316eeb
Yi Wen Wong <wongyw86@gmail.com>
2024-10-03 05:44:54 -0700

    initial project skeleton based on scala

---
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinDispatcher.java
@@ -0,0 +1,4 @@
+package org.apache.fury.serializer.kotlin;
+
+public class KotlinDispatcher {
+}
---
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
@@ -0,0 +1,13 @@
+package org.apache.fury.serializer.kotlin;
+
+import org.apache.fury.Fury;
+import org.apache.fury.serializer.Serializer;
+import org.apache.fury.serializer.SerializerFactory;
+
+@SuppressWarnings({"rawtypes"})
+public class KotlinSerializers implements SerializerFactory {
+    @Override
+    public Serializer createSerializer(Fury fury, Class<?> clz) {
+        return null;
+    }
+}
