a2d07cfc48657c38a6e1740574cfe0ad3b31a551
Yi Wen Wong <wongyw86@gmail.com>
2024-10-03 07:03:00 -0700

    Put out abstract interfaces for map and collection serializers

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
@@ -1,4 +1,9 @@
 package org.apache.fury.serializer.kotlin
 
-class CollectionSerializer {
-}
+import org.apache.fury.Fury
+import org.apache.fury.serializer.collection.AbstractCollectionSerializer
+
+abstract class AbstractKotlinCollectionSerializer<A, T: Iterable<A>>(
+    fury: Fury,
+    cls: Class<T?>?
+) : AbstractCollectionSerializer<T>(fury, cls)
--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/MapSerializer.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/MapSerializer.kt
@@ -1,4 +1,9 @@
 package org.apache.fury.serializer.kotlin
 
-class MapSerializer {
-}
+import org.apache.fury.Fury
+import org.apache.fury.serializer.collection.AbstractMapSerializer
+
+abstract class AbstractKotlinMapSerializer<K, V, T : Map<K, V>>(
+    fury: Fury,
+    cls: Class<T?>?
+) : AbstractMapSerializer<T>(fury, cls)
