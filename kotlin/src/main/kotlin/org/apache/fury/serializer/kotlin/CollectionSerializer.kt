529ac462628e81167911415213190936022c209f
Yi Wen Wong <wongyw86@gmail.com>
2024-10-04 06:04:41 -0700

    collection adapter for kotlin

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
@@ -5,5 +5,22 @@ import org.apache.fury.serializer.collection.AbstractCollectionSerializer
 
 abstract class AbstractKotlinCollectionSerializer<A, T: Iterable<A>>(
     fury: Fury,
-    cls: Class<T?>?
+    cls: Class<T>
 ) : AbstractCollectionSerializer<T>(fury, cls)
+
+/**
+ * A Collection adapter which wraps a kotlin iterable into a [[java.util.Collection]].
+ *
+ *
+ */
+private class CollectionAdaptor<A>(
+    val coll: Iterable<A>
+) : java.util.AbstractCollection<A>() {
+    private val mutableList = coll.toMutableList()
+
+    override val size: Int
+        get() = coll.count()
+
+    override fun iterator(): MutableIterator<A> =
+        mutableList.iterator()
+}
