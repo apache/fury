4184a0fd7640d43ec2cba834bb140ce390ba0249
Yi Wen Wong <wongyw86@gmail.com>
2024-10-04 05:21:44 -0700

    skeleton for KotlinSerializers

--- kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
@@ -1,13 +1,31 @@
 package org.apache.fury.serializer.kotlin;
 
 import org.apache.fury.Fury;
+import org.apache.fury.resolver.ClassResolver;
 import org.apache.fury.serializer.Serializer;
 import org.apache.fury.serializer.SerializerFactory;
 
-@SuppressWarnings({"rawtypes"})
-public class KotlinSerializers implements SerializerFactory {
-    @Override
-    public Serializer createSerializer(Fury fury, Class<?> clz) {
-        return null;
+public class KotlinSerializers {
+    public static void registerSerializers(Fury fury) {
+        ClassResolver resolver = setSerializerFactory(fury);
+    }
+
+    private static ClassResolver setSerializerFactory(Fury fury) {
+        ClassResolver resolver = fury.getClassResolver();
+        KotlinDispatcher dispatcher = new KotlinDispatcher();
+        SerializerFactory factory = resolver.getSerializerFactory();
+        if (factory != null) {
+            SerializerFactory newFactory = (f, cls) -> {
+                Serializer<?> serializer = factory.createSerializer(f, cls);
+                if (serializer == null) {
+                    serializer = dispatcher.createSerializer(f, cls);
+                }
+                return serializer;
+            };
+            resolver.setSerializerFactory(newFactory);
+        } else {
+            resolver.setSerializerFactory(dispatcher);
+        }
+        return resolver;
     }
 }
