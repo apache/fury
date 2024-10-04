6d8e27cfa2f95c98feea60b35306293857c60c30
Yi Wen Wong <wongyw86@gmail.com>
2024-10-04 07:27:00 -0700

    change primary ctor to point to KClass instead of java class.

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
@@ -3,12 +3,14 @@ package org.apache.fury.serializer.kotlin
 import org.apache.fury.Fury
 import org.apache.fury.memory.MemoryBuffer
 import org.apache.fury.serializer.collection.AbstractCollectionSerializer
+import kotlin.reflect.KClass
 
 abstract class AbstractKotlinCollectionSerializer<E, T: Iterable<E>>(
     private val factory: CollectionFactory<E, T>,
     fury: Fury,
-    cls: Class<T>
-) : AbstractCollectionSerializer<T>(fury, cls) {
+    cls: KClass<T>
+) : AbstractCollectionSerializer<T>(fury, cls.java) {
+
     abstract override fun onCollectionWrite(buffer: MemoryBuffer, value: T?): Collection<E>?
 
     override fun read(buffer: MemoryBuffer): T? {
