fcf1e2843f7aff8e995548ef7c4f5011bccf0014
Yi Wen Wong <wongyw86@gmail.com>
2024-10-04 06:13:25 -0700

    skeleton for abstract kotlin collection serializer.

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializer.kt
@@ -1,12 +1,30 @@
 package org.apache.fury.serializer.kotlin
 
 import org.apache.fury.Fury
+import org.apache.fury.memory.MemoryBuffer
 import org.apache.fury.serializer.collection.AbstractCollectionSerializer
 
 abstract class AbstractKotlinCollectionSerializer<A, T: Iterable<A>>(
     fury: Fury,
     cls: Class<T>
-) : AbstractCollectionSerializer<T>(fury, cls)
+) : AbstractCollectionSerializer<T>(fury, cls) {
+    abstract override fun onCollectionWrite(buffer: MemoryBuffer, value: T?): Collection<A>?
+
+    override fun read(buffer: MemoryBuffer): T? {
+        val collection = newCollection(buffer)
+        val numElements = getAndClearNumElements()
+        if (numElements != 0) readElements(fury, buffer, collection, numElements)
+        return onCollectionRead(collection)
+    }
+
+    override fun newCollection(buffer: MemoryBuffer?): Collection<A>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCollectionRead(collection: Collection<*>?): T? {
+        TODO("Not yet implemented")
+    }
+}
 
 /**
  * A Collection adapter which wraps a kotlin iterable into a [[java.util.Collection]].
