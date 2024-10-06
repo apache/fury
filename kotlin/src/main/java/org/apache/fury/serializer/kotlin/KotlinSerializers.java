f9df84c03919fc50ff963746df50096d3b71787f
Yi Wen Wong <wongyw86@gmail.com>
2024-10-06 08:43:23 -0700

    Add EmptyList/Set/Map serializers

--- kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
@@ -2,18 +2,42 @@ package org.apache.fury.serializer.kotlin;
 
 import org.apache.fury.Fury;
 import org.apache.fury.resolver.ClassResolver;
+import org.apache.fury.serializer.ArraySerializers;
 import org.apache.fury.serializer.Serializer;
 import org.apache.fury.serializer.SerializerFactory;
+import org.apache.fury.serializer.collection.CollectionSerializers;
+import org.apache.fury.serializer.collection.MapSerializers;
+
 
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class KotlinSerializers {
     public static void registerSerializers(Fury fury) {
         ClassResolver resolver = setSerializerFactory(fury);
 
+        // Arrays in kotlin map to java arrays.
+        ArraySerializers.registerDefaultSerializers(fury);
+        // Most kotlin stdlib implementation map to Java collections.
+        CollectionSerializers.registerDefaultSerializers(fury);
+
+        // EmptyList
+        Class emptyListClass = KClassToJava.INSTANCE.getEmptyListClass();
+        resolver.register(emptyListClass);
+        resolver.registerSerializer(emptyListClass, new CollectionSerializers.EmptyListSerializer(fury, emptyListClass));
+
+        // EmptySet
+        Class emptySetClass = KClassToJava.INSTANCE.getEmptySetClass();
+        resolver.register(emptySetClass);
+        resolver.registerSerializer(emptySetClass, new CollectionSerializers.EmptySetSerializer(fury, emptySetClass));
+
+        // EmptyMap
+        Class emptyMapClass = KClassToJava.INSTANCE.getEmptyMapClass();
+        resolver.register(emptyMapClass);
+        resolver.registerSerializer(emptyMapClass, new MapSerializers.EmptyMapSerializer(fury, emptyMapClass));
+
         // Non-Java collection implementation in kotlin stdlib.
         Class arrayDequeClass = KClassToJava.INSTANCE.getArrayDequeClass();
+        resolver.register(arrayDequeClass);
         resolver.registerSerializer(arrayDequeClass, new KotlinArrayDequeSerializer(fury, arrayDequeClass));
-        resolver.register(kotlin.collections.ArrayDeque.class);
     }
 
     private static ClassResolver setSerializerFactory(Fury fury) {
--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/KClassToJava.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/KClassToJava.kt
@@ -2,4 +2,7 @@ package org.apache.fury.serializer.kotlin
 
 object KClassToJava {
     val ArrayDequeClass = ArrayDeque::class.java
+    val EmptyListClass = emptyList<Any>()::class.java
+    val EmptySetClass = emptySet<Any>()::class.java
+    val EmptyMapClass = emptyMap<Any, Any>()::class.java
 }
