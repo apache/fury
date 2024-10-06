72408cb35dce60abdbe0b38c2c40322b9ab16b6c
Yi Wen Wong <wongyw86@gmail.com>
2024-10-06 08:43:23 -0700

    Add EmptyList/Set/Map serializers

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/MapSerializer.kt
+++
@@ -1,9 +0,0 @@
-package org.apache.fury.serializer.kotlin
-
-import org.apache.fury.Fury
-import org.apache.fury.serializer.collection.AbstractMapSerializer
-
-abstract class AbstractKotlinMapSerializer<K, V, T : Map<K, V>>(
-    fury: Fury,
-    cls: Class<T?>?
-) : AbstractMapSerializer<T>(fury, cls)
--- kotlin/src/test/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializerTest.kt
+++ kotlin/src/test/kotlin/org/apache/fury/serializer/kotlin/CollectionSerializerTest.kt
@@ -11,21 +11,42 @@ class CollectionSerializerTest {
         val fury: Fury = Fury.builder()
             .withLanguage(Language.XLANG)
             .withRefTracking(true)
-            .requireClassRegistration(false)
+            .requireClassRegistration(true)
             .suppressClassRegistrationWarnings(false)
             .build()
 
         KotlinSerializers.registerSerializers(fury)
-        val list = ArrayDeque(listOf(1,2,3,4,5))
+        val arrayDeque = ArrayDeque(listOf(1,2,3,4,5))
+        assertEquals (arrayDeque, fury.deserialize(fury.serialize(arrayDeque)))
+
+        val arrayList = arrayListOf(1,2,3,4,5)
+        assertEquals (arrayList, fury.deserialize(fury.serialize(arrayList)))
+
+        val emptyList = listOf<Int>()
+        assertEquals (emptyList, fury.deserialize(fury.serialize(emptyList)))
+
+        val list = listOf(1,2,3,4,5)
         assertEquals (list, fury.deserialize(fury.serialize(list)))
 
-        val list2 = listOf(1,2,3,4,5)
-        assertEquals (list2, fury.deserialize(fury.serialize(list2)))
+        val mutableList = mutableListOf(1,2,3,4,5)
+        assertEquals (mutableList, fury.deserialize(fury.serialize(mutableList)))
+
+        val emptySet = setOf<Int>()
+        assertEquals (emptySet, fury.deserialize(fury.serialize(emptySet)))
 
         val set = setOf(1,2,3,4,5)
         assertEquals (set, fury.deserialize(fury.serialize(set)))
 
+        val mutableSet = mutableSetOf(1,2,3,4,5)
+        assertEquals (mutableSet, fury.deserialize(fury.serialize(mutableSet)))
+
+        val emptyMap: Map<Int, String> = mapOf()
+        assertEquals (emptyMap, fury.deserialize(fury.serialize(emptyMap)))
+
         val map = mapOf(1 to "one",2 to "two",3 to "three")
         assertEquals (map, fury.deserialize(fury.serialize(map)))
+
+        val mutableMap = mapOf(1 to "one",2 to "two",3 to "three")
+        assertEquals (map, fury.deserialize(fury.serialize(mutableMap)))
     }
 }
