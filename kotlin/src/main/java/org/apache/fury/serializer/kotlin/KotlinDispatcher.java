fb781964f543a4aa00b0b30a1e23bdf10fb1d785
Yi Wen Wong <wongyw86@gmail.com>
2024-10-07 07:18:02 -0700

    use serializer instead of dispatcher

--- kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinDispatcher.java
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinDispatcher.java
@@ -7,18 +7,10 @@ import org.apache.fury.serializer.SerializerFactory;
 /**
  * Serializer dispatcher for kotlin types.
  */
-@SuppressWarnings({"rawtypes", "unchecked"})
+@SuppressWarnings({"rawtypes"})
 public class KotlinDispatcher implements SerializerFactory {
     @Override
     public Serializer createSerializer(Fury fury, Class<?> clz) {
-        Class mapWithDefaultClass = KotlinToJavaClass.INSTANCE.getMapWithDefaultClass();
-        Class mutableMapWithDefaultClass = KotlinToJavaClass.INSTANCE.getMutableMapWitDefaultClass();
-
-        if (mapWithDefaultClass.isAssignableFrom(clz) ||
-        mutableMapWithDefaultClass.isAssignableFrom(clz)) {
-            return new MapWithDefaultSerializer(fury, clz);
-        }
-
         return null;
     }
 }
