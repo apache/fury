9cbdfe5d9be328f4af173d80389b83ecb41cb5ec
Yi Wen Wong <wongyw86@gmail.com>
2024-10-08 06:26:09 -0700

    add licenses

--- kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionBuilder.kt
+++ kotlin/src/main/kotlin/org/apache/fury/serializer/kotlin/CollectionBuilder.kt
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.fury.serializer.kotlin
 
 interface CollectionBuilder <E, T: Iterable<E>> {
@@ -13,10 +31,6 @@ abstract class AbstractListBuilder<E, T:List<E>>
     override fun add(element: E) =
         builder.add(element)
     override fun iterator(): MutableIterator<E> = builder.iterator()
-}
-
-class ListBuilder<E>: AbstractListBuilder<E, MutableList<E>>() {
-    override fun result(): MutableList<E> = builder
 }
 
 class ArrayDequeBuilder<E>(
