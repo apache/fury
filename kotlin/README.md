0673c7b82ed5cf522d7c5b18551a8fd73efacaa1
Yi Wen Wong <3934946+wywen@users.noreply.github.com>
2024-10-14 08:20:51 -0700

    feat(kotlin): Introduce kotlin package with stdlib collections and tests (#1877)
    ## What does this PR do?
    
    This PR adds a base project for Kotlin on the JVM.
    - It adds a pom.xml based on the java project.
    - Adds a Serializer class that registers base collection classes not
    covered by the JVM.
    - Adds code to test all documented collections in the kotlin stdlib via
    TestNG.
    - Adds register and adds serializers for EmptyList, EmptySet, EmptyMap
    classes.
    - This PR does not handle kotlin `withDefault` wrapper classes, as that
    requires serializing arbitrary lambdas.
    
    ## Related issues
    
    - #683
    
    ## Does this PR introduce any user-facing change?
    
    This PR creates a new fury-kotlin JAR, no documentation yet from the
    main project.
    
    - [ ] Does this PR introduce any public API change?
    - [ ] Does this PR introduce any binary protocol compatibility change?
    
    ## Benchmark
    N/A
    
    ---------
    
    Co-authored-by: Shawn Yang <chaokunyang@apache.org>

---
+++ kotlin/README.md
@@ -0,0 +1,21 @@
+# Apache Fury™ Kotlin
+
+This provides additional Fury support for Kotlin Serialization on JVM:
+
+Most standard kotlin types are already supported out of the box with the default Fury java implementation.
+
+Fury Kotlin provides additional tests and implementation support for Kotlin types.
+
+Fury Kotlin is tested and works with the following types:
+
+- stdlib `collection`: `ArrayDeque`, `ArrayList`, `HashMap`,`HashSet`, `LinkedHashSet`, `LinkedHashMap`.
+- `ArrayList`, `HashMap`,`HashSet`, `LinkedHashSet`, `LinkedHashMap` works out of the box with the default java implementation.
+
+Additional support is added for the following classes in kotlin:
+
+- Empty collections: `emptyList`, `emptyMap`, `emptySet`
+- Collections: `ArrayDeque`
+
+Additional Notes:
+
+- wrappers classes created from `withDefault` method is currently not supported.
---
+++ kotlin/src/main/java/org/apache/fury/serializer/kotlin/KotlinSerializers.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fury.serializer.kotlin;
+
+import org.apache.fury.Fury;
+import org.apache.fury.resolver.ClassResolver;
+import org.apache.fury.serializer.collection.CollectionSerializers;
+import org.apache.fury.serializer.collection.MapSerializers;
+
+
+/**
+ * KotlinSerializers provide default serializers for kotlin.
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class KotlinSerializers {
+    public static void registerSerializers(Fury fury) {
+        ClassResolver resolver = fury.getClassResolver();
+
+        // EmptyList
+        Class emptyListClass = KotlinToJavaClass.INSTANCE.getEmptyListClass();
+        resolver.register(emptyListClass);
+        resolver.registerSerializer(emptyListClass, new CollectionSerializers.EmptyListSerializer(fury, emptyListClass));
+
+        // EmptySet
+        Class emptySetClass = KotlinToJavaClass.INSTANCE.getEmptySetClass();
+        resolver.register(emptySetClass);
+        resolver.registerSerializer(emptySetClass, new CollectionSerializers.EmptySetSerializer(fury, emptySetClass));
+
+        // EmptyMap
+        Class emptyMapClass = KotlinToJavaClass.INSTANCE.getEmptyMapClass();
+        resolver.register(emptyMapClass);
+        resolver.registerSerializer(emptyMapClass, new MapSerializers.EmptyMapSerializer(fury, emptyMapClass));
+
+        // Non-Java collection implementation in kotlin stdlib.
+        Class arrayDequeClass = KotlinToJavaClass.INSTANCE.getArrayDequeClass();
+        resolver.register(arrayDequeClass);
+        resolver.registerSerializer(arrayDequeClass, new KotlinArrayDequeSerializer(fury, arrayDequeClass));
+    }
+}
