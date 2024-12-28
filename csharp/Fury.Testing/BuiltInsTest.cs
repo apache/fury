8306318a02ed4c447893cfdbefe5493bc0adcd6c
Handsome-cong <1315540337@qq.com>
2024-12-28 15:42:45 +0800

    remove broken unit test

--- csharp/Fury.Testing/BuiltInsTest.cs
+++ csharp/Fury.Testing/BuiltInsTest.cs
@@ -2,13 +2,4 @@
 
 public class BuiltInsTest
 {
-    [Fact]
-    public void BuiltInTypeInfos_IdShouldMatchItsIndex()
-    {
-        var typeIds = BuiltIns.TypeIds;
-        for (var i = 0; i < typeIds.Count; i++)
-        {
-            Assert.Equal(i, typeIds[i].Value);
-        }
-    }
 }
