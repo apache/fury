1f57495b28a6df083a903b177eb84bc0550677ae
Junduo Dong <andj4cn@gmail.com>
2025-01-20 13:53:22 +0800

    add numpy memoryview test
    Signed-off-by: Junduo Dong <andj4cn@gmail.com>

--- python/pyfury/tests/test_serializer.py
+++ python/pyfury/tests/test_serializer.py
@@ -249,6 +249,24 @@ def test_array_serializer(language):
         np.testing.assert_array_equal(new_arr, arr)
 
 
+def test_numpy_array_memoryview();
+    _WINDOWS = os.name == "nt"
+    if _WINDOWS:
+        arr = np.array(list(range(10)), dtype='int32')
+        assert arr.format == 'l'
+        assert arr.itemsize == 4
+        arr = np.array(list(range(10)), dtype='int64')
+        assert arr.format == 'q'
+        assert arr.itemsize == 8
+    else:
+        arr = np.array(list(range(10)), dtype='int32')
+        assert arr.format == 'i'
+        assert arr.itemsize == 4
+        arr = np.array(list(range(10)), dtype='int64')
+        assert arr.format == 'l'
+        assert arr.itemsize == 8
+
+
 def ser_de(fury, obj):
     binary = fury.serialize(obj)
     return fury.deserialize(binary)
