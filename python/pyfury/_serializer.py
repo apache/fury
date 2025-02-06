df1599f0697b007ee3d9914879c5a84f10034df5
chaokunyang <shawn.ck.yang@gmail.com>
2025-02-06 22:39:40 +0800

    removed fastpath

--- python/pyfury/_serializer.py
+++ python/pyfury/_serializer.py
@@ -401,35 +401,11 @@ class MapSerializer(Serializer):
                 ):
                     break
                 if not key_write_ref or not ref_resolver.write_ref_or_null(buffer, key):
-                    if key_cls is str:
-                        buffer.write_string(key)
-                    elif key_serializer_type is Int64Serializer:
-                        buffer.write_varint64(key)
-                    elif key_serializer_type is Float64Serializer:
-                        buffer.write_double(key)
-                    elif key_serializer_type is Int32Serializer:
-                        buffer.write_varint32(key)
-                    elif key_serializer_type is Float32Serializer:
-                        buffer.write_float(key)
-                    else:
-                        key_serializer.write(buffer, key)
+                    key_serializer.write(buffer, key)
                 if not value_write_ref or not ref_resolver.write_ref_or_null(
                     buffer, value
                 ):
-                    if value_cls is str:
-                        buffer.write_string(value)
-                    elif value_serializer_type is Int64Serializer:
-                        buffer.write_varint64(value)
-                    elif value_serializer_type is Float64Serializer:
-                        buffer.write_double(value)
-                    elif value_serializer_type is Int32Serializer:
-                        buffer.write_varint32(value)
-                    elif value_serializer_type is Float32Serializer:
-                        buffer.write_float(value)
-                    elif value_serializer_type is BooleanSerializer:
-                        buffer.write_bool(value)
-                    else:
-                        value_serializer.write(buffer, value)
+                    value_serializer.write(buffer, value)
                 chunk_size += 1
                 if pos >= len(items) or chunk_size == MAX_CHUNK_SIZE:
                     break
@@ -515,18 +491,7 @@ class MapSerializer(Serializer):
                         key = key_serializer.read(buffer)
                         ref_resolver.set_read_object(ref_id, key)
                 else:
-                    if key_serializer_type is StringSerializer:
-                        key = buffer.read_string()
-                    elif key_serializer_type is Int64Serializer:
-                        key = buffer.read_varint64()
-                    elif key_serializer_type is Float64Serializer:
-                        key = buffer.read_double()
-                    elif key_serializer_type is Int32Serializer:
-                        key = buffer.read_varint32()
-                    elif key_serializer_type is Float32Serializer:
-                        key = buffer.read_float()
-                    else:
-                        key = key_serializer.read(buffer)
+                    key = key_serializer.read(buffer)
                 if track_value_ref:
                     ref_id = ref_resolver.try_preserve_ref_id(buffer)
                     if ref_id < NOT_NULL_VALUE_FLAG:
@@ -535,20 +500,7 @@ class MapSerializer(Serializer):
                         value = value_serializer.read(buffer)
                         ref_resolver.set_read_object(ref_id, value)
                 else:
-                    if value_serializer_type is StringSerializer:
-                        value = buffer.read_string()
-                    elif value_serializer_type is Int64Serializer:
-                        value = buffer.read_varint64()
-                    elif value_serializer_type is Float64Serializer:
-                        value = buffer.read_double()
-                    elif value_serializer_type is Int32Serializer:
-                        value = buffer.read_varint32()
-                    elif value_serializer_type is Float32Serializer:
-                        value = buffer.read_float()
-                    elif value_serializer_type is BooleanSerializer:
-                        value = buffer.read_bool()
-                    else:
-                        value = value_serializer.read(buffer)
+                    value = value_serializer.read(buffer)
                 map_[key] = value
                 size -= 1
             if size != 0:
