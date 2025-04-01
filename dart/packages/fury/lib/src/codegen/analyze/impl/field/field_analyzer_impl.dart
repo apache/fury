9cbcd6c5fc6e395f95c95bb7cb3ae853841890f4
leo <lskleo@163.com>
2025-04-02 01:17:52 +0800

    [Dart] add license header for all dart file

--- dart/packages/fury/lib/src/codegen/analyze/impl/field/field_analyzer_impl.dart
+++ dart/packages/fury/lib/src/codegen/analyze/impl/field/field_analyzer_impl.dart
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 // Each field will have a setter and getter, this does not refer to the public getter and setter in the OOP concept
 // Even private fields will have private getters and setters, they are just not visible externally
 // Similar to if a setter is defined using the set keyword, for example
